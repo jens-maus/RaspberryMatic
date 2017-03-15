7789524920a01260fff6aad0eb1866778fe47a5f
Jens Maus <mail@jens-maus.de>
2017-03-15 20:53:46 +0100

    set OCCU version to official stable '2.27.7' tag.

--- buildroot-external/package/homematic/homematic.mk
+++ buildroot-external/package/homematic/homematic.mk
@@ -5,7 +5,7 @@
 #############################################################
 
 HOMEMATIC_OCCU_VERSION = 2.27.7
-HOMEMATIC_VERSION = 532edbdf01dae7a595323a318711e14853124539
+HOMEMATIC_VERSION = 2.27.7
 HOMEMATIC_SITE = $(call github,eq-3,occu,$(HOMEMATIC_VERSION))
 
 HOMEMATIC_MODULE_SUBDIRS = kernel-modules/bcm2835_raw_uart kernel-modules/eq3_char_loop

05f264f0f22d6893f1e2f76a61dea3e6cf68863a
Jens Maus <mail@jens-maus.de>
2017-03-15 14:50:23 +0100

    updated libfirewall.tcl with the latest version available in CCU2 2.27.7 update.

--- buildroot-external/overlay/base/lib/libfirewall.tcl
+++ buildroot-external/overlay/base/lib/libfirewall.tcl
@@ -24,7 +24,7 @@ set Firewall_CONFIG_FILE /etc/config/firewall.conf
 #           none      : Kein Zugriff möglich
 ##
 array set Firewall_SERVICES {}
-set Firewall_SERVICES(XMLRPC) [list PORTS [list 2000 2001 2002] ACCESS full]
+set Firewall_SERVICES(XMLRPC) [list PORTS [list 2000 2001 2002 2010 9292] ACCESS full]
 set Firewall_SERVICES(REGA)   [list PORTS [list 8181] ACCESS restricted]
 
 ##
@@ -42,59 +42,67 @@ set Firewall_IPS [list 192.168.0.1 192.168.0.0/16]
 # Lädt die Firwall-Einstellunfen aus der Konfigurationsdatei
 ##
 proc Firewall_loadConfiguration { } {
-	global Firewall_CONFIG_FILE Firewall_SERVICES Firewall_IPS
-	
-	array set config  {}
-	array set section {}
-	set sectionName   {}
-	
-	if { ![catch { set fd [open $Firewall_CONFIG_FILE r] }] } then {
-		catch {
-			while { ![eof $fd] } {
-
-				set line [string trimleft [gets $fd]]
-
-				if { "\#" != [string index $line 0] } then {
-
-					if { [regexp {\[([^\]]+)\]} $line dummy newSectionName] } then {
-					set config($sectionName) [array get section]
-						array_clear section
-						set sectionName $newSectionName
-					}
+  global Firewall_CONFIG_FILE Firewall_SERVICES Firewall_IPS
   
-					if { [regexp {([^=]+)=(.+)} $line dummy name value] } then {
-						set section([string trim $name]) [string trim $value]
-					}
+  array set config  {}
+  array set section {}
+  set sectionName   {}
+  
+  if { ![catch { set fd [open $Firewall_CONFIG_FILE r] }] } then {
+    catch {
+      while { ![eof $fd] } {
 
-				}      
+        set line [string trimleft [gets $fd]]
+
+        if { "\#" != [string index $line 0] } then {
+
+          if { [regexp {\[([^\]]+)\]} $line dummy newSectionName] } then {
+          set config($sectionName) [array get section]
+            array_clear section
+            set sectionName $newSectionName
+          }
+  
+          if { [regexp {([^=]+)=(.+)} $line dummy name value] } then {
+            set section([string trim $name]) [string trim $value]
+          }
+
+        }      
     
-			}
-			set config($sectionName) [array get section]
-			array_clear section
-		}
-	
-		close $fd
-	}
-	
-	foreach sectionName [array names config] {
-		if { {} != $sectionName } then {
-			array set section $config($sectionName)
-			
-			set id     [array_getValue section Id]
-			set ports  [array_getValue section Ports]
-			set access [array_getValue section Access]
-			
-			set Firewall_SERVICES($id) [list PORTS $ports ACCESS $access]
-			
-			array_clear section
-		}
-	}
+      }
+      set config($sectionName) [array get section]
+      array_clear section
+    }
+  
+    close $fd
+  }
+  
+  foreach sectionName [array names config] {
 
-	if { [info exists config()] } then {
-		array set emptySection $config()
-		set Firewall_IPS [array_getValue emptySection IPs]
-	}
-	
+    if { {} != $sectionName } then {
+      array set section $config($sectionName)
+      set ports  [array_getValue section Ports]
+      set id     [array_getValue section Id]
+
+      # Check if port 2010 for the service XMLRPC is set - if not add it.
+      if {$sectionName == "SERVICE XMLRPC"} {
+       if {[string first 2010 $ports] == -1} {
+           append ports " 2010"
+         }
+       }
+
+      set access [array_getValue section Access]
+      
+      set Firewall_SERVICES($id) [list PORTS [lsort -integer $ports] ACCESS $access]
+      
+      array_clear section
+    }
+  }
+
+  if { [info exists config()] } then {
+    array set emptySection $config()
+    set Firewall_IPS [array_getValue emptySection IPs]
+  }
+  
 }
 
 ##
@@ -102,31 +110,31 @@ proc Firewall_loadConfiguration { } {
 # Speichert die Firewall-Einstellungen in der Konfigurationsdatei
 ##
 proc Firewall_saveConfiguration {} {
-	global Firewall_CONFIG_FILE Firewall_SERVICES Firewall_IPS
-	
-	set fd [open $Firewall_CONFIG_FILE w]
-	catch {
-	
-		puts $fd "# Firewall Configuration file"
-		puts $fd "#   created [clock format [clock seconds]]"
-		puts $fd "# This file was automatically gernerated"
-		puts $fd "# Please do not change anything"
-		puts $fd ""
-		
-		puts $fd "IPs = $Firewall_IPS"
-		puts $fd ""
-		
-		foreach serviceName [array names Firewall_SERVICES] {
-			array set service $Firewall_SERVICES($serviceName)
-			puts $fd "\[SERVICE $serviceName\]"
-			puts $fd "Id = $serviceName"
-			puts $fd "Ports = $service(PORTS)"
-			puts $fd "Access = $service(ACCESS)"
-			puts $fd ""
-		}
-		
-	}
-		close $fd
+  global Firewall_CONFIG_FILE Firewall_SERVICES Firewall_IPS
+  
+  set fd [open $Firewall_CONFIG_FILE w]
+  catch {
+  
+    puts $fd "# Firewall Configuration file"
+    puts $fd "#   created [clock format [clock seconds]]"
+    puts $fd "# This file was automatically gernerated"
+    puts $fd "# Please do not change anything"
+    puts $fd ""
+    
+    puts $fd "IPs = $Firewall_IPS"
+    puts $fd ""
+    
+    foreach serviceName [array names Firewall_SERVICES] {
+      array set service $Firewall_SERVICES($serviceName)
+      puts $fd "\[SERVICE $serviceName\]"
+      puts $fd "Id = $serviceName"
+      puts $fd "Ports = $service(PORTS)"
+      puts $fd "Access = $service(ACCESS)"
+      puts $fd ""
+    }
+    
+  }
+    close $fd
 }
 
 ##
@@ -134,33 +142,32 @@ proc Firewall_saveConfiguration {} {
 # Konfiguriert die Firewall
 ##
 proc Firewall_configureFirewall { } {
-	global Firewall_SERVICES Firewall_IPS
-	
-	exec_cmd "/usr/sbin/iptables -F"
-	exec_cmd "/usr/sbin/iptables -P INPUT ACCEPT"
-	exec_cmd "/usr/sbin/iptables -A INPUT -i lo -j ACCEPT"
-	exec_cmd "/usr/sbin/iptables -A INPUT -p tcp --dport 8182 -j DROP"
+  global Firewall_SERVICES Firewall_IPS
+  
+  exec_cmd "/usr/sbin/iptables -F"
+  exec_cmd "/usr/sbin/iptables -P INPUT ACCEPT"
+  exec_cmd "/usr/sbin/iptables -A INPUT -i lo -j ACCEPT"
+  exec_cmd "/usr/sbin/iptables -A INPUT -p tcp --dport 8182 -j DROP"
 
-
-	foreach serviceName [array names Firewall_SERVICES] {
-		array set service $Firewall_SERVICES($serviceName)
-	
-		if { $service(ACCESS) == "restricted" } then {
-			foreach port $service(PORTS) {
-				foreach ip $Firewall_IPS {
-					exec_cmd "/usr/sbin/iptables -A INPUT -p tcp --dport $port -s $ip -j ACCEPT"
-				}
-			}
-		}
-		
-		if { $service(ACCESS) == "none" || $service(ACCESS) == "restricted"} then {
-			foreach port $service(PORTS) {
-				exec_cmd "/usr/sbin/iptables -A INPUT -p tcp --dport $port -j DROP"
-			}
-		}
-	
-	}
-	
+  foreach serviceName [array names Firewall_SERVICES] {
+    array set service $Firewall_SERVICES($serviceName)
+  
+    if { $service(ACCESS) == "restricted" } then {
+      foreach port $service(PORTS) {
+        foreach ip $Firewall_IPS {
+          exec_cmd "/usr/sbin/iptables -A INPUT -p tcp --dport $port -s $ip -j ACCEPT"
+        }
+      }
+    }
+    
+    if { $service(ACCESS) == "none" || $service(ACCESS) == "restricted"} then {
+      foreach port $service(PORTS) {
+        exec_cmd "/usr/sbin/iptables -A INPUT -p tcp --dport $port -j DROP"
+      }
+    }
+  
+  }
+  
 }
 
 ##
@@ -168,8 +175,8 @@ proc Firewall_configureFirewall { } {
 # Führt eine Kommandozeile aus
 ##
 proc exec_cmd {cmdline} {
-	set fd [open "|$cmdline" r]
-	close $fd
+  set fd [open "|$cmdline" r]
+  close $fd
 }
 
 ##
@@ -177,10 +184,10 @@ proc exec_cmd {cmdline} {
 # Löscht die Elemente in einem Array
 ##
 proc array_clear {name} {
-	upvar $name arr
-	foreach key [array names arr] {
-		unset arr($key)
-	}
+  upvar $name arr
+  foreach key [array names arr] {
+    unset arr($key)
+  }
 }
 
 ##

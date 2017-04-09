b2f66379a87379e914008b909cfe4b1943b23b29
Jens Maus <mail@jens-maus.de>
2017-04-09 21:32:13 +0200

    updated occu commit id to latest master branch version.

--- buildroot-external/package/homematic/homematic.mk
+++ buildroot-external/package/homematic/homematic.mk
@@ -5,7 +5,7 @@
 #############################################################
 
 HOMEMATIC_OCCU_VERSION = 2.27.8
-HOMEMATIC_VERSION = 65eed4a646f4a86141e334d6c9f7471f9d56481c
+HOMEMATIC_VERSION = b89342ff338d2958316a3130b11d58d41207b1ac
 HOMEMATIC_SITE = $(call github,eq-3,occu,$(HOMEMATIC_VERSION))
 
 HOMEMATIC_MODULE_SUBDIRS = kernel-modules/bcm2835_raw_uart kernel-modules/eq3_char_loop

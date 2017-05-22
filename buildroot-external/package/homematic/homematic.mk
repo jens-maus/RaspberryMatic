c442565756dcd69764cbb11a6459424464fc8543
Jens Maus <mail@jens-maus.de>
2017-05-23 00:25:03 +0200

    updated OCCU commit id to latest master branch changes (ReGaHss beta10)

--- buildroot-external/package/homematic/homematic.mk
+++ buildroot-external/package/homematic/homematic.mk
@@ -5,7 +5,7 @@
 #############################################################
 
 HOMEMATIC_OCCU_VERSION = 2.27.8
-HOMEMATIC_VERSION = 8d2e2bbf30a943fb979d18c4cc945ce6480804d1
+HOMEMATIC_VERSION = 334e81fbdbdaf361dad17008ed87eb0cf0b0f699
 HOMEMATIC_SITE = $(call github,eq-3,occu,$(HOMEMATIC_VERSION))
 
 HOMEMATIC_MODULE_SUBDIRS = kernel-modules/bcm2835_raw_uart kernel-modules/eq3_char_loop

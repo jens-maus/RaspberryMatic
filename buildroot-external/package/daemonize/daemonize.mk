e22d665ceb35df863907469fa7f6828d52d75ca5
Jens Maus <mail@jens-maus.de>
2017-06-01 23:03:45 +0200

    added --prefix=/ to daemonize package config to install the 'daemonize' binary to /sbin

--- buildroot-external/package/daemonize/daemonize.mk
+++ buildroot-external/package/daemonize/daemonize.mk
@@ -9,4 +9,6 @@ DAEMONIZE_SITE = $(call github,bmc,daemonize,release-$(DAEMONIZE_VERSION))
 DAEMONIZE_LICENSE = BSD-2c
 DAEMONIZE_LICENSE_FILES = LICENSE.md
 
+DAEMONIZE_CONF_OPTS = --prefix=/
+
 $(eval $(autotools-package))

200a34437d9c63a3c8cedaa69907e56647ac8875
Jens Maus <mail@jens-maus.de>
2017-06-14 20:53:05 +0200

    added U-Boot configuration stuff for tinkerboard. This enabled the board to finally boot the first time. This refs #111.

--- buildroot-external/board/tinkerboard/post-image.sh
+++ buildroot-external/board/tinkerboard/post-image.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+MKIMAGE=${HOST_DIR}/usr/bin/mkimage
 BOARD_DIR="$(dirname $0)"
 BOARD_NAME="$(basename ${BOARD_DIR})"
 GENIMAGE_CFG="${BR2_EXTERNAL_RASPBERRYMATIC_PATH}/board/${BOARD_NAME}/genimage.cfg"
@@ -8,6 +9,10 @@ GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
 # Use our extlinux.conf and hw_intf.conf
 cp "${BR2_EXTERNAL_RASPBERRYMATIC_PATH}/board/${BOARD_NAME}/extlinux.conf" "${BINARIES_DIR}/"
 cp "${BR2_EXTERNAL_RASPBERRYMATIC_PATH}/board/${BOARD_NAME}/hw_intf.conf" "${BINARIES_DIR}/"
+
+# prepare the uboot image to be flashed
+${MKIMAGE} -n rk3288 -T rksd -d ${BINARIES_DIR}/u-boot-spl-dtb.bin ${BINARIES_DIR}/u-boot.bin
+cat ${BINARIES_DIR}/u-boot-dtb.bin >>${BINARIES_DIR}/u-boot.bin
 
 #
 # Create user filesystem

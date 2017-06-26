f11608190be9675742b33cfb13600b5b98215eff
Jens Maus <mail@jens-maus.de>
2017-06-26 11:48:49 +0200

    reworked checkFactoryReset() function in S00eQ3SystemStart to generate a rootfs2 with equal size like rootfs1 and also userfs completely on demand upon the first boot of a fresh install. This should help to integrate the still missing webui-based firmware update functionality in one of the next releases. This refs #16.

--- buildroot-external/board/raspberrypi0/post-image.sh
+++ buildroot-external/board/raspberrypi0/post-image.sh
@@ -9,17 +9,7 @@ GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
 cp "${BR2_EXTERNAL_RASPBERRYMATIC_PATH}/board/${BOARD_NAME}/cmdline.txt" "${BINARIES_DIR}/rpi-firmware/"
 cp "${BR2_EXTERNAL_RASPBERRYMATIC_PATH}/board/${BOARD_NAME}/config.txt" "${BINARIES_DIR}/rpi-firmware/"
 
-#
-# Create user filesystem
-#
-echo "Create user filesystem"
-mkdir -p ${BUILD_DIR}/userfs
-touch "${BUILD_DIR}/userfs/.doFactoryReset"
-mke2img -d ${BUILD_DIR}/userfs -G 4 -R 1 -b 3000 -l userfs -o ${BINARIES_DIR}/userfs.ext4
-
-#
 # VERSION File
-#
 cp ${TARGET_DIR}/boot/VERSION ${BINARIES_DIR}
 
 rm -rf "${GENIMAGE_TMP}"

7cdd91b606689fd6210c3566eb04298994c995e7
Jens Maus <mail@jens-maus.de>
2017-06-27 13:57:27 +0200

    update CloudMatic package to latest master branch commit (4964e9cf81b5bb76016a89b044194168de28025a) and removed obsolete RaspberryMatic-patch from our repository.

--- buildroot-external/package/cloudmatic/cloudmatic.mk
+++ buildroot-external/package/cloudmatic/cloudmatic.mk
@@ -4,15 +4,15 @@
 #
 #############################################################
 CLOUDMATIC_VERSION = 2017020901
-CLOUDMATIC_COMMIT = aad53fa975b7ce05c4e81d1434ea7bb3e1826db5
+CLOUDMATIC_COMMIT = 4964e9cf81b5bb76016a89b044194168de28025a
 CLOUDMATIC_SITE = $(call github,EasySmartHome,CloudMatic-CCUAddon,$(CLOUDMATIC_COMMIT))
 CLOUDMATIC_LICENSE = PROPERITARY
 
 define CLOUDMATIC_INSTALL_TARGET_CMDS
-	mkdir $(TARGET_DIR)/opt/mh
+	mkdir -p $(TARGET_DIR)/opt/mh
 	cp -a $(@D)/install.tcl $(TARGET_DIR)/opt/mh/
 	cp -a $(@D)/startup.sh $(TARGET_DIR)/opt/mh/
-	ln -s /usr/sbin/openvpn $(TARGET_DIR)/opt/mh/
+	cp -a $(@D)/openvpn $(TARGET_DIR)/opt/mh/
 	cp -a $(@D)/user $(TARGET_DIR)/opt/mh/
 	cp -a $(@D)/www $(TARGET_DIR)/opt/mh/
 endef

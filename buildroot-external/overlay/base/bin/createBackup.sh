746018cfe65fced3b79cd90c307ee7634ea9a225
Jens Maus <mail@jens-maus.de>
2017-09-02 19:12:54 +0200

    added "--one-file-system --ignore-failed-read" to backup tar creation as well as "--user=root --group=root" which should make backup archive creation more security and less error prone. This refs #150.

--- buildroot-external/overlay/base/bin/createBackup.sh
+++ buildroot-external/overlay/base/bin/createBackup.sh
@@ -18,7 +18,7 @@ TMPDIR=$(mktemp -d -p /usr/local/tmp)
 echo 'load tclrega.so; rega system.Save()' | tclsh 2>&1 >/dev/null
 
 # create a gzipped tar of /usr/local
-tar --exclude=/usr/local/tmp --exclude=/usr/local/lost+found -czf ${TMPDIR}/usr_local.tar.gz /usr/local 2>/dev/null
+tar --owner=root --group=root --exclude=/usr/local/tmp --exclude=/usr/local/lost+found --one-file-system --ignore-failed-read -czf ${TMPDIR}/usr_local.tar.gz /usr/local 2>/dev/null
 
 # sign the configuration with the current key
 crypttool -s -t 1 <${TMPDIR}/usr_local.tar.gz >${TMPDIR}/signature
@@ -31,7 +31,7 @@ source /boot/VERSION 2>/dev/null
 cp /boot/VERSION ${TMPDIR}/firmware_version
 
 # create sbk file
-tar -C ${TMPDIR} -cf ${BACKUPDIR}/"$(hostname)-${VERSION}-$(date +%Y-%m-%d-%H%M).sbk" usr_local.tar.gz signature key_index firmware_version 2>/dev/null
+tar -C ${TMPDIR} --owner=root --group=root -cf ${BACKUPDIR}/"$(hostname)-${VERSION}-$(date +%Y-%m-%d-%H%M).sbk" usr_local.tar.gz signature key_index firmware_version 2>/dev/null
 
 # remove all temp files
 rm -rf ${TMPDIR}

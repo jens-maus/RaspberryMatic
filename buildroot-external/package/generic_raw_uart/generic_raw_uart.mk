aa4ab6e30f9bdd1b060b047ed9109ffa5295d610
Jens Maus <mail@jens-maus.de>
2018-02-04 14:30:11 +0100

    create all device tree overlays via BUILD_CMDS and install them via IMAGES_CMDS

--- buildroot-external/package/generic_raw_uart/generic_raw_uart.mk
+++ buildroot-external/package/generic_raw_uart/generic_raw_uart.mk
@@ -8,8 +8,23 @@ GENERIC_RAW_UART_VERSION = 1.2
 GENERIC_RAW_UART_TAG = 1bec8b24bdf9b1b3a2511a0f1b888eb9f59724dd
 GENERIC_RAW_UART_SITE = $(call github,alexreinert,piVCCU,$(GENERIC_RAW_UART_TAG))
 GENERIC_RAW_UART_LICENSE = GPL
+GENERIC_RAW_UART_DEPENDENCIES = host-dtc
 #GENERIC_RAW_UART_LICENSE_FILES = LICENSE
 GENERIC_RAW_UART_MODULE_SUBDIRS = kernel
+GENERIC_RAW_UART_INSTALL_IMAGES = YES
+
+define GENERIC_RAW_UART_BUILD_CMDS
+  for dts in $(@D)/dts/*.dts; do \
+    $(HOST_DIR)/bin/dtc -@ -I dts -O dtb -W no-unit_address_vs_reg -o $${dts%.dts}.dtbo $${dts}; \
+  done
+endef
+
+define GENERIC_RAW_UART_INSTALL_IMAGES_CMDS
+  mkdir -p $(BINARIES_DIR)/overlays; \
+  for dtbo in $(@D)/dts/*.dtbo; do \
+    $(INSTALL) -D -m 0644 $${dtbo} $(BINARIES_DIR)/overlays/; \
+  done
+endef
 
 $(eval $(kernel-module))
 $(eval $(generic-package))

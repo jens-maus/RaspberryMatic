558070f3a4894392927b4bd09d946395eac25afa
Jens Maus <mail@jens-maus.de>
2018-02-12 11:31:12 +0100

    updated commit id of generic-raw-uart to catch latest changes and added references to the author of that raw-uart kernel module.

--- buildroot-external/package/generic_raw_uart/generic_raw_uart.mk
+++ buildroot-external/package/generic_raw_uart/generic_raw_uart.mk
@@ -2,9 +2,12 @@
 #
 # Generic RAW UART kernel module
 #
+# Alexander Reinert <alex@areinert.de>
+# https://github.com/alexreinert/piVCCU/tree/master/kernel
+#
 #############################################################
 
-GENERIC_RAW_UART_VERSION = raspberrymatic_tinkerboard_fix
+GENERIC_RAW_UART_VERSION = 8d0b575ead8c6f810ce6f5c4b239b5ed7b1b9d07
 GENERIC_RAW_UART_SITE = $(call github,alexreinert,piVCCU,$(GENERIC_RAW_UART_VERSION))
 GENERIC_RAW_UART_LICENSE = GPL
 GENERIC_RAW_UART_DEPENDENCIES = host-dtc

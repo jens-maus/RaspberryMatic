74f96a1d1c484e85beb157f29adb0baa05ee75e5
Jens Maus <mail@jens-maus.de>
2018-03-07 14:03:11 +0100

    added a new 'recovery-image' package which will be used to be able to boot into a dedicated recovery system in future.

---
+++ buildroot-external/package/recovery-image/external/external.mk
@@ -0,0 +1,1 @@
+include $(sort $(wildcard $(BR2_EXTERNAL_RECOVERY_IMAGE_PATH)/package/*/*.mk))
---
+++ buildroot-external/package/recovery-image/recovery-image.mk
@@ -0,0 +1,28 @@
+#############################################################
+#
+# Recovery Image Package
+#
+#############################################################
+
+RECOVERY_IMAGE_VERSION = 1.0.0
+RECOVERY_IMAGE_BUILDROOT = 2018.02
+RECOVERY_IMAGE_SITE = $(call github,buildroot,buildroot,$(RECOVERY_IMAGE_BUILDROOT))
+RECOVERY_IMAGE_LICENSE = Apache-2.0
+
+define RECOVERY_IMAGE_PRE_PATCH
+	for p in $(wildcard ../buildroot-patches/*.patch); do \
+		patch -p1 -d $(@D) <$${p}; \
+  done
+endef
+RECOVERY_IMAGE_PRE_PATCH_HOOKS += RECOVERY_IMAGE_PRE_PATCH
+
+define RECOVERY_IMAGE_BUILD_CMDS
+	$(MAKE) -C $(@D) BR2_EXTERNAL=$(RECOVERY_IMAGE_PKGDIR)/external recovery_rpi3_defconfig
+	$(MAKE) -C $(@D) BR2_EXTERNAL=$(RECOVERY_IMAGE_PKGDIR)/external
+endef
+
+define RECOVERY_IMAGE_INSTALL_TARGET_CMDS
+	cp -a $(@D)/output/images/rootfs.ext2 $(BINARIES_DIR)/recoveryfs.ext4
+endef
+
+$(eval $(generic-package))

e568825f749589b886e2796ba4046f3488d1b475
Jens Maus <mail@jens-maus.de>
2018-03-07 23:22:40 +0100

    lets boot the right partitions for recoveryfs vs rootfs.

--- buildroot-external/board/raspberrypi3/boot.cmd
+++ buildroot-external/board/raspberrypi3/boot.cmd
@@ -2,8 +2,14 @@
 gpio input GPIO12
 if test $? -eq 0; then
   echo "==== STARTING RECOVERY SYSTEM ===="
+  setenv partuuid "deedbeef-02"
 else
   echo "==== NORMAL BOOT ===="
-  fdt addr ${fdt_addr} && fdt get value bootargs /chosen bootargs
-  fatload mmc ${devnum} ${kernel_addr_r} zImage && bootz ${kernel_addr_r} - ${fdt_addr};
+  setenv partuuid "deedbeef-03"
 fi
+
+# modify bootargs, load kernel and boot it
+fdt addr ${fdt_addr} && fdt get value bootargs /chosen bootargs
+setenv bootargs "${bootargs} root=PARTUUID=${partuuid}"
+fatload mmc ${devnum} ${kernel_addr_r} zImage
+bootz ${kernel_addr_r} - ${fdt_addr};

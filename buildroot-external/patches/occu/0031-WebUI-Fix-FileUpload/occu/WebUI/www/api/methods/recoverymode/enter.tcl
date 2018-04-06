15cbf5e2f96f09b85b506fcee33d8678bc1c9056
Jens Maus <mail@jens-maus.de>
2018-04-07 01:10:17 +0200

    forgot to add new modified files in 0031 patchset.

---
+++ buildroot-external/patches/occu/0031-WebUI-Fix-FileUpload/occu/WebUI/www/api/methods/recoverymode/enter.tcl
@@ -0,0 +1,23 @@
+##
+# RecoveryMode.enter
+# Startet die HomeMatic Zentrale im Recovery Modus
+#
+# Parameter:
+#   paasword: [string] Passwort für den Recovery Modus
+#
+# Rückgabewert: [bool]
+#   true
+##
+
+set FLAG_FILE "/usr/local/.recoveryMode"
+
+# Flag-Datei erzeugen
+set fd [open $FLAG_FILE w]
+puts $fd 1
+close $fd
+
+# Neustart
+rega system.Save()
+exec /sbin/reboot
+
+jsonrpc_response true

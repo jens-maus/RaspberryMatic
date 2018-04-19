9171439f1116b616bc1fd017fb90bec9536570f4
Jens Maus <mail@jens-maus.de>
2018-04-19 18:35:29 +0200

    updated RaspberryPi linux kernel to latest stable "raspberrypi-kernel_1.20180417-1" version (4.14.34) including its corresponding rpi-firmware.

--- buildroot-patches/buildroot/package/rpi-firmware/rpi-firmware.mk
+++ buildroot-patches/buildroot/package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = ae9a493932e47e08cabb25a2728037298075fd00
+RPI_FIRMWARE_VERSION = a154f2136850dba827cf4bc40794854376902cbd
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3-Clause
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom

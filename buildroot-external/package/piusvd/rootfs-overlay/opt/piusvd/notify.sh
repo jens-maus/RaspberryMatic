2ad2cf75d11117a7e6e8ceb1f4029ebbbe710a19
Jens Maus <mail@jens-maus.de>
2018-05-20 00:30:49 +0200

    added notify.sh script and added it to the piupsmon.conf config file so that an alarm is triggered as soon as power loss is identified with a PiUSV+

---
+++ buildroot-external/package/piusvd/rootfs-overlay/opt/piusvd/notify.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+PIUPSMON_LOG=/var/log/piupsmon.log
+STATUS_CHANGE=$(tail -1 ${PIUPSMON_LOG} | sed -n 's/.* Status changed from \(.\) .* to \(.\) .*/\1 \2/p')
+if [[ -n "${STATUS_CHANGE}" ]]; then
+  STATUS_FROM=$(echo ${STATUS_CHANGE} | cut -d' ' -f1)
+  STATUS_TO=$(echo ${STATUS_CHANGE} | cut -d' ' -f2)
+  MESSAGE=
+
+  case "${STATUS_TO}" in
+    2)
+      MESSAGE="PiUSV+: Power Loss"
+    ;;
+
+    9)
+      MESSAGE="PiUSV+: Power Restored"
+    ;;
+  esac
+
+  if [[ -n "${MESSAGE}" ]]; then
+    /bin/triggerAlarm.tcl "${MESSAGE}"
+  fi
+fi

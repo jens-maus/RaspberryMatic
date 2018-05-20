7f1c743db79d0a5003bc940cc6e00a52b0f94fe8
Jens Maus <mail@jens-maus.de>
2018-05-20 11:19:25 +0200

    applied minor fix to piusvd notify.sh script identifying the actual UPS event upon power loss/restore.

--- buildroot-external/package/piusvd/rootfs-overlay/opt/piusvd/notify.sh
+++ buildroot-external/package/piusvd/rootfs-overlay/opt/piusvd/notify.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 PIUPSMON_LOG=/var/log/piupsmon.log
-STATUS_CHANGE=$(tail -1 ${PIUPSMON_LOG} | sed -n 's/.* Status changed from \(.\) .* to \(.\) .*/\1 \2/p')
+STATUS_CHANGE=$(grep "Status changed from" ${PIUPSMON_LOG} | tail -1 ${PIUPSMON_LOG} | sed -n 's/.* Status changed from \(.\) .* to \(.\) .*/\1 \2/p')
 if [[ -n "${STATUS_CHANGE}" ]]; then
   STATUS_FROM=$(echo ${STATUS_CHANGE} | cut -d' ' -f1)
   STATUS_TO=$(echo ${STATUS_CHANGE} | cut -d' ' -f2)

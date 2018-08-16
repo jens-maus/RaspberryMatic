daa5ef0ea3ec3814b3d20346bebdbc5bdce47f2c
Jens Maus <mail@jens-maus.de>
2018-08-16 22:00:50 +0200

    bumped version of raspberrypi linux kernel and corresponding rpi-firmware to latest 4.14.62 to potentially fix random kernel panic issues reported lately. This refs #393.

--- buildroot-patches/buildroot/package/rpi-firmware/rpi-firmware.mk
+++ buildroot-patches/buildroot/package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = 469f2d8eb88a4ef71f77d4912b8f15ba3a7dd57b
+RPI_FIRMWARE_VERSION = ebbecf992a466f9bffbd9e3f443f56f64994a277
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3-Clause
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom

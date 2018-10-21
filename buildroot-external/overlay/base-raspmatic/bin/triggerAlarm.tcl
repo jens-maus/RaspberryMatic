697de7756d17b5d9ec37d4e3edcbfe5629ea694a
Jens Maus <mail@jens-maus.de>
2018-10-21 18:18:19 +0200

    reworked /bin/triggerAlarm.tcl to actually create the "Alarmzone 1" system variable if it is doesn not exist anymore. This script can also take an additional argument with a alarm variable name now and generate/use that instead. This should allow to have arbitrary alarm messages/variables now and not rely on "Alarmzone 1" only anymore.

--- buildroot-external/overlay/base-raspmatic/bin/triggerAlarm.tcl
+++ buildroot-external/overlay/base-raspmatic/bin/triggerAlarm.tcl
@@ -1,9 +1,10 @@
 #!/bin/tclsh
 #
-# Simple tclsh script for triggering an alarm message at the first
-# ALARMDP system variable with name "Alarmzone 1".
+# Simple tclsh script for triggering an alarm message with DPInfo
+# <msg> to alarm variable <var> with eventually creating it.
 #
-# Copyright (c) 2017 Jens Maus <mail@jens-maus.de>
+# triggerAlarm.ctl v2.0
+# Copyright (c) 2017-2018 Jens Maus <mail@jens-maus.de>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,59 +19,88 @@
 # limitations under the License.
 #
 # Usage:
-# triggerAlarm.tcl <msg>
+# triggerAlarm.tcl <msg> <var>
 #
 
 load tclrpc.so
 load tclrega.so
 
-if { $argc != 1 } {
-  puts "ERROR: script requires exactly one argument"
-  return
+if { $argc < 1 } {
+  puts "ERROR: script requires one or more arguments"
+  exit 1
 }
 
-# function to return a list of alarm variables
-proc getAlarmZoneVariableID { } {
-  set script "
-    string sSysVarId;
-    foreach (sSysVarId, dom.GetObject(ID_SYSTEM_VARIABLES).EnumUsedIDs()) {
-      object oSysVar = dom.GetObject(sSysVarId);
-      string sValueType = oSysVar.ValueType();
+set msg [lindex $argv 0]
+set var [lindex $argv 1]
 
-      if(oSysVar.TypeName() == 'ALARMDP') {
-        WriteLine(sSysVarId # ';' # oSysVar.Name());
+# if no additional variable name was given
+# we search for a SV with name "Alarmzone 1" or
+# "${sysVarAlarmZone1}"
+if { $var == "" } {
+  set script "
+    object alObj = null;
+    string sSysVarId;
+    foreach(sSysVarId, dom.GetObject(ID_SYSTEM_VARIABLES).EnumUsedIDs()) {
+      object oSysVar = dom.GetObject(sSysVarId);
+      if((alObj == null) &&
+        (oSysVar.TypeName() == \"ALARMDP\") &&
+        ((oSysVar.Name() == \"\${sysVarAlarmZone1}\") || (oSysVar.Name() == \"Alarmzone 1\"))) {
+        alObj=oSysVar;
       }
     }
   "
-
-  if { ![catch {array set result [rega_script $script]}] } then {
-    set variables [split $result(STDOUT) "\n"]
-    foreach subsection $variables {
-      set b [split $subsection ";"]
-      set sysVarId [lindex $b 0]
-      set sysVarName [string tolower [lindex $b 1]]
-      if {[regexp ".*alarmzone.*1" $sysVarName]} {
-        return $sysVarId
+  set var "\${sysVarAlarmZone1}"
+} else {
+  set script "
+    object alObj = null;
+    string sSysVarId;
+    foreach(sSysVarId, dom.GetObject(ID_SYSTEM_VARIABLES).EnumUsedIDs()) {
+      object oSysVar = dom.GetObject(sSysVarId);
+      if((alObj == null) &&
+         (oSysVar.Name() == \"$var\") &&
+         (oSysVar.TypeName() == \"ALARMDP\")) {
+        alObj=oSysVar;
       }
     }
-  }
-  return ""
+  "
 }
 
-proc triggerAlarm { msg } {
-  set sysVarAlarmZone1 [getAlarmZoneVariableID]
-  if { [string length $sysVarAlarmZone1] > 0 } then {
-    set script "
-      dom.GetObject('$sysVarAlarmZone1').State(true);
-      dom.GetObject('$sysVarAlarmZone1').DPInfo('$msg');
-    "
-
-    if { ![catch {array set result [rega_script $script]}] } then {
-      return true
+# try to get an alarm variable with name $var
+# or create it if it does not exist
+append script "
+  if(alObj == null) {
+    alObj=dom.CreateObject(OT_ALARMDP);
+    if(alObj != null) {
+      alObj.Name(\"$var\");
+      alObj.ValueType(ivtBinary);
+      alObj.ValueSubType(istAlarm);
+      alObj.ValueName0(\"\${sysVarAlarmZone1NotTriggered}\");
+      alObj.ValueName1(\"\${sysVarAlarmZone1Triggered}\");
+      alObj.ValueUnit(\"\");
+      alObj.AlType(atSystem);
+      alObj.AlArm(true);
+      alObj.State(false);
+      dom.GetObject(ID_SYSTEM_VARIABLES).Add(alObj.ID());
+      dom.RTUpdate(1);
     }
   }
 
-  return false
+  if(alObj != null) {
+    alObj.ValueType(ivtBinary);
+    alObj.ValueSubType(istAlarm);
+    alObj.DPInfo(\"$msg\");
+    alObj.State(true);
+  }
+"
+
+if { ![catch {array set result [rega_script $script]}] } then {
+  if { $result(alObj) != "null" } {
+    set res 0
+  } else {
+    set res 1
+  }
+} else {
+  set res 1
 }
 
-set res [triggerAlarm [lindex $argv 0]]
+exit $res

31367d11e826ab574fb3fd3171f52a98bf6946e9
Jens Maus <mail@jens-maus.de>
2018-11-09 17:22:59 +0100

    lets use the 3.41.7 tagged version of jens-maus/occu

--- buildroot-external/package/occu/occu.mk
+++ buildroot-external/package/occu/occu.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-OCCU_VERSION = f1a713609068d7b819d55bfefbef0c9ce7781b25
+OCCU_VERSION = 3.41.7
 OCCU_SITE = $(call github,jens-maus,occu,$(OCCU_VERSION))
 
 ifeq ($(BR2_PACKAGE_OCCU),y)

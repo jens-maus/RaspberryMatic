fe00e407831705b42d38ab8c6dde45c5971dc42b
Jens Maus <mail@jens-maus.de>
2018-12-12 22:49:26 +0100

    bumped OCCU to 3.41.11-6 with latest ReGaHss R1.00.0388.0204 beta version included and applied WebUI index.htm change

--- buildroot-external/package/occu/occu.mk
+++ buildroot-external/package/occu/occu.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-OCCU_VERSION = 3.41.11-5
+OCCU_VERSION = 3.41.11-6
 OCCU_SITE = $(call github,jens-maus,occu,$(OCCU_VERSION))
 
 ifeq ($(BR2_PACKAGE_OCCU),y)

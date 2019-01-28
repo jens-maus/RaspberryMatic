57d1e1aefd51ca643995a1620cda1b3cc5129108
Jens Maus <mail@jens-maus.de>
2019-01-28 11:50:54 +0100

    slightly reworked restoreBackup.sh to check all included archive checksums before actually starting a backup restore. In addition, it also makes sure now to really only extract files in the embedded usr_local archive that are located in /usr/local. This should prevent from accidently overwriting certain things in the rootfs or other filesystem just because a user incorrectly patched the backup archive.

--- buildroot-external/overlay/base-raspmatic/bin/restoreBackup.sh
+++ buildroot-external/overlay/base-raspmatic/bin/restoreBackup.sh
@@ -2,7 +2,7 @@
 #
 # simple wrapper script to restore a standard sbk file backup
 #
-# Copyright (c) 2016-2018 Jens Maus <mail@jens-maus.de>
+# Copyright (c) 2016-2019 Jens Maus <mail@jens-maus.de>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -29,26 +29,63 @@ if [ -z "${1}" ]; then
 fi
 
 TMPDIR=$(mktemp -d -p ${BACKUPDIR})
+if [[ -d "${TMPDIR}" ]]; then
 
-# extract the sbk to /usr/local/tmp
-tar -C ${TMPDIR} -xf ${BACKUPFILE}
+  # extract the sbk to /usr/local/tmp
+  tar -C "${TMPDIR}" -xf "${BACKUPFILE}"
 
-# make sure all relevant services are stopped
-/etc/init.d/S70ReGaHss stop
-/etc/init.d/S62HMServer stop
-/etc/init.d/S61rfd stop
-/etc/init.d/S60hs485d stop
-/etc/init.d/S50lighttpd stop
-/sbin/start-stop-daemon -K -q -p /var/run/crond.pid
+  # check archive consistency using sha256
+  if [[ "${TMPDIR}/signature.sha256" ]]; then
+    (cd "${TMPDIR}"; sha256sum -s -c signature.sha256)
+    if [[ $? -ne 0 ]]; then
+      echo "ERROR: inconsistent backup archive identified (sha256)."
+      rm -rf "${TMPDIR}"
+      exit 1
+    fi
+  fi
 
-# wait some time to get all daemons time to finish
-sleep 5
+  # check archive consistency using crypttool
+  CUR_SIGNATURE=$(crypttool -s -t 0 <"${TMPDIR}/usr_local.tar.gz")
+  OLD_SIGNATURE=$(cat "${TMPDIR}/signature")
+  if [[ -z "${CUR_SIGNATURE}" || "${CUR_SIGNATURE}" != "${OLD_SIGNATURE}" ]]; then
+    echo "ERROR: inconsistent backup archive identified (crypttool)."
+    rm -rf "${TMPDIR}"
+    exit 1
+  fi
 
-# now remove the whole /usr/local, but keep /usr/local/tmp
-find /usr/local -not -name tmp -not -name "lost+found" -mindepth 1 -maxdepth 1 -exec rm -rf {} \;
+  # make sure all relevant services are stopped
+  /etc/init.d/S98crond stop
+  /etc/init.d/S70ReGaHss stop
+  /etc/init.d/S62HMServer stop
+  /etc/init.d/S61rfd stop
+  /etc/init.d/S60hs485d stop
+  /etc/init.d/S50lighttpd stop
 
-# extract usr_local.tar.gz
-tar -C / --warning=no-timestamp --no-same-owner -xf ${TMPDIR}/usr_local.tar.gz
+  # wait some time to get all daemons time to finish
+  sleep 5
 
-# remove all temp files
-rm -rf ${TMPDIR}
+  # now remove the whole /usr/local, but keep /usr/local/tmp
+  find /usr/local -xdev -not -name tmp -not -name "lost+found" -mindepth 1 -maxdepth 1 -exec rm -rf {} \;
+
+  # extract usr_local.tar.gz but make sure NOT to unarchive anything outside
+  # /usr/local or this might overwrite unwanted things if present in the archive
+  tar -C /usr/local --strip-components=2 --warning=no-timestamp --no-same-owner -xf "${TMPDIR}/usr_local.tar.gz" usr/local
+
+  # remove all temp files
+  rm -rf "${TMPDIR}"
+
+  # sync filesystem
+  sync
+
+  # make sure all relevant services are started again
+  /etc/init.d/S50lighttpd start
+  /etc/init.d/S60hs485d start
+  /etc/init.d/S61rfd start
+  /etc/init.d/S62HMServer start
+  /etc/init.d/S70ReGaHss start
+  /etc/init.d/S98crond start
+
+  exit 0
+else
+  exit 1
+fi

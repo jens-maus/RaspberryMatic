3b2b3eb3c2225015f03ed52c0801ce401d092757
Jens Maus <mail@jens-maus.de>
2019-02-12 09:02:21 +0100

    bumped java version to latest java11 (LTS) variant and adapted install script accordingly.

--- buildroot-external/package/java-azul/java-azul.mk
+++ buildroot-external/package/java-azul/java-azul.mk
@@ -5,19 +5,23 @@
 # http://www.azul.com/downloads/zulu-embedded/
 #
 #############################################################
-JAVA_AZUL_VERSION = 8.33.0.134-jdk1.8.0_192
+JAVA_AZUL_VERSION = 11.1.8-ca-jdk11-c2
 JAVA_AZUL_SOURCE=zulu$(JAVA_AZUL_VERSION)-linux_aarch32hf.tar.gz
 JAVA_AZUL_SITE = http://cdn.azul.com/zulu-embedded/bin
 JAVA_AZUL_DEPENDENCIES = fontconfig dejavu liberation
 
-define JAVA_AZUL_PRE_PATCH
-	cp $(JAVA_AZUL_PKGDIR)/Makefile $(@D)
-endef
-
-JAVA_AZUL_PRE_PATCH_HOOKS += JAVA_AZUL_PRE_PATCH
-
 define JAVA_AZUL_INSTALL_TARGET_CMDS
-	$(MAKE) -C $(@D) install 
+	mkdir -p $(TARGET_DIR)/opt/java-azul
+	cp -a $(@D)/bin $(TARGET_DIR)/opt/java-azul/
+	cp -a $(@D)/lib $(TARGET_DIR)/opt/java-azul/
+	rm -f $(TARGET_DIR)/opt/java-azul/lib/src.zip
+	cp -a $(@D)/jmods $(TARGET_DIR)/opt/java-azul/
+	cp -a $(@D)/conf $(TARGET_DIR)/opt/java-azul/
+	cp -a $(@D)/legal $(TARGET_DIR)/opt/java-azul/
+	cp -a $(@D)/LICENSE $(TARGET_DIR)/opt/java-azul/
+	cp -a $(@D)/DISCLAIMER $(TARGET_DIR)/opt/java-azul/
+	rm -f $(TARGET_DIR)/opt/java
+	ln -s java-azul $(TARGET_DIR)/opt/java
 endef
 
 $(eval $(generic-package))

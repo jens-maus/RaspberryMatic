e146c16997abd78eff6a4da740d84d06d679d6bc
Jens Maus <mail@jens-maus.de>
2019-02-27 15:54:01 +0100

    fix the permissions for the /etc/monitrc file on each post-build.sh so that monit does not complain about incorrect permissions for the control file.

--- buildroot-external/board/raspberrypi0/post-build.sh
+++ buildroot-external/board/raspberrypi0/post-build.sh
@@ -9,6 +9,9 @@ echo "VERSION=${VERSION}" >${TARGET_DIR}/VERSION
 echo "PRODUCT=${PRODUCT}" >>${TARGET_DIR}/VERSION
 echo "PLATFORM=rpi0" >>${TARGET_DIR}/VERSION
 
+# fix some permissions
+[ -e ${TARGET_DIR}/etc/monitrc ] && chmod 600 ${TARGET_DIR}/etc/monitrc
+
 # link VERSION in /boot on rootfs
 mkdir -p ${TARGET_DIR}/boot
 ln -sf ../VERSION ${TARGET_DIR}/boot/VERSION

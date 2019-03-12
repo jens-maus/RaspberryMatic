9f7eaf9ae35318b451b5666fde4a94c5a5f44fb8
Jens Maus <mail@jens-maus.de>
2019-03-12 12:28:23 +0100

    bumped RaspberryPi linux kernel and firmware to 4.19.x line and set it to 4.19.27.

--- buildroot-patches/buildroot/package/rpi-firmware/rpi-firmware.mk
+++ buildroot-patches/buildroot/package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = 83977fe3b6ef54c1d29c83b0a778d330f523441f
+RPI_FIRMWARE_VERSION = fd15e0700e45d9b7db83e30696aba299b9f2f31d
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3-Clause
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom

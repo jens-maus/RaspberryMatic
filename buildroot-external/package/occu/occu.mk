eedfabfaccfe37c88962f9a5bbe88e0361addac7
Jens Maus <mail@jens-maus.de>
2019-03-29 08:41:55 +0100

    added removal of /etc/init.d/S01syslogd and /etc/init.d/S02klogd which is now automatically added by buildroot but we don't need them since we have our own syslog init script "S07logging".

--- buildroot-external/package/occu/occu.mk
+++ buildroot-external/package/occu/occu.mk
@@ -56,6 +56,8 @@ ifeq ($(BR2_PACKAGE_OCCU),y)
 		# remove obsolete init.d jobs
 		rm -f $(TARGET_DIR)/etc/init.d/S01logging
 		rm -f $(TARGET_DIR)/etc/init.d/S20urandom
+		rm -f $(TARGET_DIR)/etc/init.d/S01syslogd
+		rm -f $(TARGET_DIR)/etc/init.d/S02klogd
 
 		# remove obsolete config templates
 		rm -f $(TARGET_DIR)/etc/config_templates/hmip_networkkey.conf

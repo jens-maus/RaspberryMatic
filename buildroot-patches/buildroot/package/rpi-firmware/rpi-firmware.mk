69abfc3c7fb645fedc75003656d2d6c74aa9f10f
Jens Maus <mail@jens-maus.de>
2019-04-26 12:31:43 +0200

    bumped raspberrypi kernel+firwmare to latest 4.19.36 version.

--- buildroot-patches/buildroot/package/rpi-firmware/rpi-firmware.mk
+++ buildroot-patches/buildroot/package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = 9ce09b817068671faddc56b5400c1b33232eabcf
+RPI_FIRMWARE_VERSION = 37affd69b66c52426747d34bc05eda0ba2f61088
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3-Clause
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom

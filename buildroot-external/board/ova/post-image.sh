fad959cf503d7d91dd63cb55b7e8acf2a7ed3759
Jens Maus <mail@jens-maus.de>
2019-10-30 11:12:29 +0100

    adapted template.ovf to match VirtualBox+vmWare ESXi at the same time and adapted post-image.sh to actually create a valid *.ova archive which we can distribute to users then.

--- buildroot-external/board/ova/post-image.sh
+++ buildroot-external/board/ova/post-image.sh
@@ -30,4 +30,13 @@ ${HOST_DIR}/bin/qemu-img convert -O vmdk -o subformat=streamOptimized ${BINARIES
 ${HOST_DIR}/bin/qemu-img convert -O vhdx ${BINARIES_DIR}/sdcard.img ${BINARIES_DIR}/sdcard.vhdx
 ${HOST_DIR}/bin/qemu-img convert -O vdi ${BINARIES_DIR}/sdcard.img ${BINARIES_DIR}/sdcard.vdi
 
+# lets create an *.ova archive from the vmdk, template ovf file and
+# also create a *.mf manifest file
+OVADIR=$(mktemp -d)
+cp -a ${BINARIES_DIR}/sdcard.vmdk ${OVADIR}/RaspberryMatic.vmdk
+cp -a ${BOARD_DIR}/template.ovf ${OVADIR}/RaspberryMatic.ovf
+(cd ${OVADIR}; sha1sum --tag RaspberryMatic.* >RaspberryMatic.mf)
+tar -C ${OVADIR} --owner=root --group=root -cf ${BINARIES_DIR}/RaspberryMatic.ova RaspberryMatic.vmdk RaspberryMatic.ovf RaspberryMatic.mf
+#rm -rf ${OVADIR}
+
 exit $?

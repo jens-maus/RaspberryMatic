aa1eff37f46acf016fd73cfd5868f8e9d176e121
Jens Maus <mail@jens-maus.de>
2019-11-06 22:34:11 +0100

    modified intelnuc defconfig to produce a first working image.

--- buildroot-external/board/intelnuc/post-build.sh
+++ buildroot-external/board/intelnuc/post-build.sh
@@ -7,7 +7,7 @@ cp -a ${BINARIES_DIR}/bzImage ${TARGET_DIR}/zImage
 VERSION=$(cat ${BR2_EXTERNAL_EQ3_PATH}/../VERSION)
 echo "VERSION=${VERSION}" >${TARGET_DIR}/VERSION
 echo "PRODUCT=${PRODUCT}" >>${TARGET_DIR}/VERSION
-echo "PLATFORM=intel-nuc" >>${TARGET_DIR}/VERSION
+echo "PLATFORM=intelnuc" >>${TARGET_DIR}/VERSION
 
 # fix some permissions
 [ -e ${TARGET_DIR}/etc/monitrc ] && chmod 600 ${TARGET_DIR}/etc/monitrc
--- buildroot-external/package/recovery-system/external/board/intelnuc/post-build.sh
+++ buildroot-external/package/recovery-system/external/board/intelnuc/post-build.sh
@@ -4,7 +4,7 @@
 VERSION="${BR2_RECOVERY_SYSTEM_VERSION}"
 echo "VERSION=${VERSION}" >${TARGET_DIR}/VERSION
 echo "PRODUCT=${PRODUCT}" >>${TARGET_DIR}/VERSION
-echo "PLATFORM=intel-nuc" >>${TARGET_DIR}/VERSION
+echo "PLATFORM=intelnuc" >>${TARGET_DIR}/VERSION
 
 # Define parameters with default values
 DHCP_VENDOR_ID=eQ3-CCU3

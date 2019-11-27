7514cf8a258858d6851f0fd46660cefa55f44065
Jens Maus <mail@jens-maus.de>
2019-11-27 09:47:50 +0100

    updated CloudMatic package to latest version available and integrated mechanisms to use i686 binaries of nginx for the reverse-proxy functionality to work with our x86-based platforms as well.

--- buildroot-external/package/cloudmatic/cloudmatic.mk
+++ buildroot-external/package/cloudmatic/cloudmatic.mk
@@ -3,13 +3,18 @@
 # CloudMatic/meine-homematic.de support
 #
 #############################################################
-CLOUDMATIC_VERSION = 76274aa77bb494aac3b4bfbc6c6e89d382852a96
+CLOUDMATIC_VERSION = 4216d1955a288d34c9d1a9dad861da3165eedc24
 CLOUDMATIC_SITE = $(call github,jens-maus,CloudMatic-CCUAddon,$(CLOUDMATIC_VERSION))
 CLOUDMATIC_LICENSE = PROPERITARY
 
 ifeq ($(BR2_arm),y)
 	NGINX_BIN=nginx.pi
 	ZABBIX_BIN=zabbix_agentd
+endif
+
+ifeq ($(BR2_i386),y)
+	NGINX_BIN=nginx.i686
+	ZABBIX_BIN=zabbix_agentd.i686
 endif
 
 define CLOUDMATIC_INSTALL_TARGET_CMDS
@@ -20,7 +25,7 @@ define CLOUDMATIC_INSTALL_TARGET_CMDS
   ln -s /usr/sbin/openvpn $(TARGET_DIR)/opt/mh/
   cp -a $(@D)/user $(TARGET_DIR)/opt/mh/
   cp -a $(@D)/www $(TARGET_DIR)/opt/mh/
-  rm -f $(TARGET_DIR)/opt/mh/user/nginx.pi $(TARGET_DIR)/opt/mh/user/nginx $(TARGET_DIR)/opt/mh/user/zabbix_agentd
+  rm -f $(TARGET_DIR)/opt/mh/user/nginx.pi $(TARGET_DIR)/opt/mh/user/nginx $(TARGET_DIR)/opt/mh/user/nginx.i686 $(TARGET_DIR)/opt/mh/user/zabbix_agentd $(TARGET_DIR)/opt/mh/user/zabbix_agentd.i686
   [[ -n "$(NGINX_BIN)" ]] && $(INSTALL) -m 0755 $(@D)/user/$(NGINX_BIN) $(TARGET_DIR)/opt/mh/user/nginx || true
   [[ -n "$(ZABBIX_BIN)" ]] && $(INSTALL) -m 0755 $(@D)/user/$(ZABBIX_BIN) $(TARGET_DIR)/opt/mh/user/zabbix_agend || true
   $(INSTALL) -D -m 0755 $(CLOUDMATIC_PKGDIR)/S97CloudMatic $(TARGET_DIR)/etc/init.d

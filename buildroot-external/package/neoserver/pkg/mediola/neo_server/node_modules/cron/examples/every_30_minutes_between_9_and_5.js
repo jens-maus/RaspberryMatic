2b8c2d3bf5c4ed987bbad8f66f01861ccdd28e39
Jens Maus <mail@jens-maus.de>
2020-06-02 20:34:50 +0200

    updated integrated neoserver addon package to latest 2.5.2 version and adapted buildroot package update script to new archive layout.

--- buildroot-external/package/neoserver/pkg/mediola/neo_server/node_modules/cron/examples/every_30_minutes_between_9_and_5.js
+++
@@ -1,9 +0,0 @@
-const CronJob = require('../lib/cron.js').CronJob;
-
-console.log('Before job instantiation');
-const job = new CronJob('0 */30 9-17 * * *', function() {
-	const d = new Date();
-	console.log('Every 30 minutes between 9-17:', d);
-});
-console.log('After job instantiation');
-job.start();
--- buildroot-external/package/neoserver/pkg/mediola/neo_server/node_modules/node-localstorage/register.js
+++
@@ -1,4 +0,0 @@
-if (typeof localStorage === "undefined" || localStorage === null) {
-  var LocalStorage = require('node-localstorage').LocalStorage;
-  global.localStorage = new LocalStorage('./scratch');
-}

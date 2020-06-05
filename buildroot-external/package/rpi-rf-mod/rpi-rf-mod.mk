aa21ebc62f22148de82320c98c98a657595f2778
Jens Maus <mail@jens-maus.de>
2020-06-05 11:41:45 +0200

    integrated a fix for the rpi-rf-mod-tinker.dts file to get it compatibly to kernel 5.4+ versions and to correctly disable uart2,uart3 and i2s so that the gpio-leds of the RPI-RF-MOD still work as expected with kernel 5.4

--- buildroot-external/package/rpi-rf-mod/rpi-rf-mod.mk
+++ buildroot-external/package/rpi-rf-mod/rpi-rf-mod.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-RPI_RF_MOD_VERSION = 1.3.0
+RPI_RF_MOD_VERSION = 1.4.0
 RPI_RF_MOD_SITE = $(BR2_EXTERNAL_EQ3_PATH)/package/rpi-rf-mod
 RPI_RF_MOD_SITE_METHOD = local
 

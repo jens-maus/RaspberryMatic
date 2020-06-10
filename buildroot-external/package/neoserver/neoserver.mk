501e001cccd717836e000109f0064948acadd61f
Jens Maus <mail@jens-maus.de>
2020-06-10 15:27:03 +0200

    updated integrated medioa neoserver addon to version 2.5.3

--- buildroot-external/package/neoserver/neoserver.mk
+++ buildroot-external/package/neoserver/neoserver.mk
@@ -3,7 +3,7 @@
 # Neo Server
 #
 #############################################################
-NEOSERVER_VERSION = 2.5.2
+NEOSERVER_VERSION = 2.5.3
 NEOSERVER_SOURCE =
 NEOSERVER_LICENSE = PROPERITARY
 

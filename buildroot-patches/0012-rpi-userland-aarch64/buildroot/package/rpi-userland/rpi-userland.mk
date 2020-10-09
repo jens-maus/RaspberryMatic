cbe42e5c5fb1b4bffffa12f301e984b2e5ae4827
Jens Maus <mail@jens-maus.de>
2020-10-09 16:12:28 +0200

    added buildroot patch to enable rpi-userland package also for aarch64 target.

---
+++ buildroot-patches/0012-rpi-userland-aarch64/buildroot/package/rpi-userland/rpi-userland.mk
@@ -0,0 +1,49 @@
+################################################################################
+#
+# rpi-userland
+#
+################################################################################
+
+RPI_USERLAND_VERSION = 188d3bfe4a0ac36b119a2cee35a6be8d0c68e09e
+RPI_USERLAND_SITE = $(call github,raspberrypi,userland,$(RPI_USERLAND_VERSION))
+RPI_USERLAND_LICENSE = BSD-3-Clause
+RPI_USERLAND_LICENSE_FILES = LICENCE
+RPI_USERLAND_INSTALL_STAGING = YES
+RPI_USERLAND_CONF_OPTS = -DVMCS_INSTALL_PREFIX=/usr
+
+RPI_USERLAND_PROVIDES = libegl libgles libopenmax libopenvg
+
+ifeq ($(BR2_PACKAGE_RPI_USERLAND_HELLO),y)
+
+RPI_USERLAND_CONF_OPTS += -DALL_APPS=ON
+
+define RPI_USERLAND_EXTRA_LIBS_TARGET
+	$(INSTALL) -m 0644 -D \
+		$(@D)/build/lib/libilclient.so \
+		$(TARGET_DIR)/usr/lib/libilclient.so
+endef
+RPI_USERLAND_POST_INSTALL_TARGET_HOOKS += RPI_USERLAND_EXTRA_LIBS_TARGET
+
+define RPI_USERLAND_EXTRA_LIBS_STAGING
+	$(INSTALL) -m 0644 -D \
+		$(@D)/build/lib/libilclient.so \
+		$(STAGING_DIR)/usr/lib/libilclient.so
+endef
+RPI_USERLAND_POST_INSTALL_STAGING_HOOKS += RPI_USERLAND_EXTRA_LIBS_STAGING
+
+else
+
+RPI_USERLAND_CONF_OPTS += -DALL_APPS=OFF
+
+endif # BR2_PACKAGE_RPI_USERLAND_HELLO
+
+define RPI_USERLAND_POST_TARGET_CLEANUP
+	rm -Rf $(TARGET_DIR)/usr/src
+endef
+RPI_USERLAND_POST_INSTALL_TARGET_HOOKS += RPI_USERLAND_POST_TARGET_CLEANUP
+
+ifeq ($(BR2_aarch64),y)
+RPI_USERLAND_CONF_OPTS += -DARM64=ON
+endif
+
+$(eval $(cmake-package))

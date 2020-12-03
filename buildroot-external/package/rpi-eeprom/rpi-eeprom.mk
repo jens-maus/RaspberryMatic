e8d088f390c8204c93c5d28b2d292cdb5e30643a
Jens Maus <mail@jens-maus.de>
2020-12-03 10:03:34 +0100

    updated pi4 eeprom version to latest 2020-11-24 beta.

--- buildroot-external/package/rpi-eeprom/rpi-eeprom.mk
+++ buildroot-external/package/rpi-eeprom/rpi-eeprom.mk
@@ -3,12 +3,12 @@
 # rpi-eeprom
 #
 #############################################################
-RPI_EEPROM_VERSION = 16bb29427f96dc8276a7102c0526154a1084bffd
+RPI_EEPROM_VERSION = 3702e706aff73a0b0ad545223f9aa4b7d98aeb5c
 RPI_EEPROM_SITE = $(call github,raspberrypi,rpi-eeprom,$(RPI_EEPROM_VERSION))
 RPI_EEPROM_LICENSE = BSD-3-Clause
 RPI_EEPROM_LICENSE_FILES = LICENSE
 RPI_EEPROM_INSTALL_IMAGES = YES
-RPI_EEPROM_FIRMWARE_PATH = firmware/beta/pieeprom-2020-10-28.bin
+RPI_EEPROM_FIRMWARE_PATH = firmware/beta/pieeprom-2020-11-24.bin
 
 define RPI_EEPROM_BUILD_CMDS
 	$(@D)/rpi-eeprom-config $(@D)/$(RPI_EEPROM_FIRMWARE_PATH) --out $(@D)/default.conf

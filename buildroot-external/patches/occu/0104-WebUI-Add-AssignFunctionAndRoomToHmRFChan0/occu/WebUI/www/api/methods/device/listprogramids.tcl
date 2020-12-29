793738f6953632d95538b902fe0e9c24f1546bd4
Jérôme <jp112sdl@icloud.com>
2020-12-29 23:41:55 +0100

    add patch 0104-WebUI-Add-AssignFunctionAndRoomToHmRFChan0 (#1040)
    * added patch 0104-WebUI-Add-AssignFunctionAndRoomToHmRFChan0
    Co-authored-by: Jens Maus <mail@jens-maus.de>

---
+++ buildroot-external/patches/occu/0104-WebUI-Add-AssignFunctionAndRoomToHmRFChan0/occu/WebUI/www/api/methods/device/listprogramids.tcl
@@ -0,0 +1,45 @@
+##
+# Device.listProgramIds
+# Liefert eine Liste der Ids aller Programme, die mindestens einen
+# Kanal des Geräts verwenden.
+#
+# Parameter:
+#   id: [string] Id des Geräts
+#
+# Rückgabewert: [array]
+#  Die Ids der entsprechenden Programme
+##
+
+set script {
+  var device = dom.GetObject(id);
+  var first  = true;
+
+  string channelId;
+  foreach(channelId, device.Channels())
+  {
+    var channel = dom.GetObject(channelId);
+    if (channel)
+    {
+      string programId;
+      foreach(programId, channel.ChnEnumDPUsagePrograms())
+      {
+        if (true != first) { Write(" "); } else { first = false; }
+        Write("{" # programId # "}");
+      }
+    }
+  }
+}
+
+set ids [rmDoublePrgIds [hmscript $script args]]
+
+set result "\["
+set first 1
+
+foreach id $ids {
+  if {$first != 1} then { append result ","} else { set first 0 }
+  append result [json_toString $id ]
+}
+
+append result "\]"
+
+jsonrpc_response $result

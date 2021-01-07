dd7e900f9db49be7004b5d879aa3ae67895c4b19
Jens Maus <mail@jens-maus.de>
2021-01-07 23:42:42 +0100

    remove more unnecessary stuff not required for the oci/docker platform. This refs #786

--- buildroot-external/board/oci/post-build.sh
+++ buildroot-external/board/oci/post-build.sh
@@ -15,6 +15,12 @@ rm -f ${TARGET_DIR}/etc/init.d/S21rngd
 rm -f ${TARGET_DIR}/etc/init.d/S31bluetooth
 #rm -f ${TARGET_DIR}/etc/init.d/S40network
 rm -f ${TARGET_DIR}/etc/init.d/S49chrony
+rm -f ${TARGET_DIR}/lib/udev/rules.d/usbmount.rules
+rm -f ${TARGET_DIR}/lib/udev/rules.d/76-usb-gadget.rules
+rm -f ${TARGET_DIR}/lib/udev/rules.d/99-hmip-rfusb.rules
+rm -rf ${TARGET_DIR}/usr/share/usbmount
+rm -rf ${TARGET_DIR}/etc/usbmount
+rm -rf ${TARGET_DIR}/etc/usb_modeswitch.d
 rm -f ${TARGET_DIR}/bin/dhcp.script
 
 #Remove files managed by docker

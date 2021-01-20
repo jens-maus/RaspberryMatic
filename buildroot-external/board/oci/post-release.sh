27f174d82d7f86eef15d50508de4faa89a54df89
Jens Maus <mail@jens-maus.de>
2021-01-20 17:52:59 +0100

    don't put update_script and EULA files in oci archives are we don't support WebUI-based updates in the oci platform anyway.

--- buildroot-external/board/oci/post-release.sh
+++ buildroot-external/board/oci/post-release.sh
@@ -10,11 +10,9 @@ BOARD=$(echo "${PRODUCT}" | cut -d'_' -f2-)
 
 # copy prepared docker image to release path
 cp -a "build-${PRODUCT}/images/RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tar" ./release/
-(cd "./release/updatepkg/${PRODUCT}" && tar --numeric-owner --owner=root --group=root -rvf "../../RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tar" update_script EULA.de EULA.en)
+#(cd "./release/updatepkg/${PRODUCT}" && tar --numeric-owner --owner=root --group=root -rvf "../../RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tar" update_script EULA.de EULA.en)
 gzip -f "./release/RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tar"
 mv "./release/RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tar.gz" "./release/RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tgz"
 (cd ./release && sha256sum "RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tgz" >"RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tgz.sha256")
-
-# copy raw rootfs.tar
 
 exit $?

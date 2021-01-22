3f7a5fc537be6126ef3711843e3af4db19201de2
Jens Maus <mail@jens-maus.de>
2021-01-22 16:21:43 +0100

    reworked deploy.sh to only request sudo/root rights if absolutely necessary (user could have docker rights as well). In addition, we only set the "--assume-yes" flag to the apt commands if explicitly requested. This should make the installation process more secure as then apt will run in interactive mode. Also slightly reworked the ci.yml.

--- buildroot-external/board/oci/deploy.sh
+++ buildroot-external/board/oci/deploy.sh
@@ -1,137 +1,204 @@
 #!/bin/bash
+#
+# Script to install the RaspberryMatic container and its dependencies
+# https://github.com/jens-maus/RaspberryMatic/wiki/en.Installation-Docker-OCI
+#
 
 # Stop on error
 set -e
-
-# Script to install the RaspberryMatic container and its dependencies
-# https://github.com/jens-maus/RaspberryMatic/wiki/en.Docker
 
 #############################################################
 #                           Settings                        #
 #############################################################
 
-#Rega Port
+# Rega Port
 : "${CCU_REGA_HTTPS_PORT:=8443}"
 
-#Rega Port
+# Rega Port
 : "${CCU_REGA_HTTP_PORT:=8080}"
 
-#ssh Port
+# SSH Port
 : "${CCU_SSH_PORT:=2222}"
 
-#Other ports to open
+# Other ports to open
 : "${CCU_PORTS_TO_OPEN:="2001 2010 8181 9292"}"
 
-#Name of the docker volume where CCU data will persist
-#It can be a local location as well such as a mounted NAS folder, cluster fs (glusterfs), etc.
+# Name of the docker volume where CCU data will persist
+# It can be a local location as well such as a mounted NAS folder, cluster fs (glusterfs), etc.
 : "${CCU_DATA_VOLUMEN:="ccu_data"}"
 
 # Container repository to use
 : "${CCU_OCI_REPO:="ghcr.io/jens-maus/raspberrymatic"}"
 
-#CCU version to use
+# CCU version to use
 : "${CCU_OCI_TAG:="latest"}"
 
-#Name of the container instance (by default use same as angelnu/ccu for easier migration)
+# Name of the container instance (by default use same as angelnu/ccu for easier migration)
 : "${CCU_CONTAINER_NAME:="ccu"}"
 
-#Additional options for docker run
+# Additional options for docker run
 : "${CCU_DOCKER_RUN_OPTIONS:=""}"
 
-#Additional options for docker pull
+# Additional options for docker pull
 : "${CCU_DOCKER_PULL_OPTIONS:=""}"
 
-#Time for a clean container stop before it gets killed
+# Time for a clean container stop before it gets killed
 : "${CCU_DOCKER_STOP_TIMEOUT:="30"}"
 
-
 #############################################################
-#                           SCRIPT                          #
+#                 DEPENDENCY INSTALLATION                   #
 #############################################################
 
-# Make sure only root can run our script
-if [[ $EUID -ne 0 ]]; then
-   echo "WARNING: This script should be run as root" 1>&2
+FORCE=
+if [[ "${1}" == "-f" ]]; then
+  FORCE="--assume-yes"
+  echo jo
 fi
 
+check_sudo() {
+  # Make sure only root can run our script
+  if [[ $EUID -ne 0 ]]; then
+    echo "ERROR: This script must be run as root/sudo"
+    exit 1
+  fi
+}
 
-#This only works on Debian/Ubuntu based OSes such as Armbian and Raspbian
-if command -v dpkg >/dev/null && ! modinfo eq3_char_loop >/dev/null 2>&1 ; then
-  echo "Installing pivcpu kernel modules"
-
-  #Add repository
-  echo "Add repository"
-  apt install -y wget
-  wget -q -O - https://www.pivccu.de/piVCCU/public.key | apt-key add -
-  bash -c 'echo "deb https://www.pivccu.de/piVCCU stable main" > /etc/apt/sources.list.d/pivccu.list'
-  apt update
-
-  #Install kernel headers
-  if command -v armbian-config >/dev/null; then
-    echo "Detected Armbian - install kernel sources and device tree"
-    apt install -y "$(dpkg --get-selections | grep 'linux-image-' | grep '\sinstall' | sed -e 's/linux-image-\([a-z0-9-]\+\).*/linux-headers-\1/')"
-    apt install -y pivccu-devicetree-armbian
-  elif grep -q Raspbian /etc/os-release; then
-    echo "Detected Raspbian - install kernel sources and raspberry modules"
-    apt install -y pivccu-modules-raspberrypi
-    echo
-    echo "NOTE: please ensure that your GPIO UART is free if you plan to connect your CCU adapter to it"
-    echo "See step 5 and 6 at https://github.com/alexreinert/piVCCU/blob/master/docs/setup/raspberrypi.md"
+pkg_installed() {
+  PKG=${1}
+  if dpkg -s "${PKG}" 2>/dev/null | grep -Eq "^Status:.*installed.*"; then
+    return 0
   else
-    echo "Uknown platform - trying generic way to install kernel headers"
-    apt install -y "linux-headers-$(uname -r)"
+    return 1
+  fi
+}
+
+# This only works on Debian/Ubuntu based OSes including Armbian and Raspbian
+if command -v dpkg >/dev/null; then
+
+  # check for docker installation
+  if ! pkg_installed docker; then
+    echo "Installing docker"
+    check_sudo
+    apt install "${FORCE}" docker
   fi
 
-  #Install & Build kernel modules
-  echo "Install & Build kernel modules"
-  apt install -y pivccu-modules-dkms
-  service pivccu-dkms start
+  # Add repository
+  if [[ ! -e /etc/apt/sources.list.d/pivccu.list ]]; then
+    echo "Adding piVCCU apt repository"
+    check_sudo
+    if ! pkg_installed wget; then
+      apt install "${FORCE}" wget
+    fi
+    wget -q -O - https://www.pivccu.de/piVCCU/public.key | apt-key add -
+    bash -c 'echo "deb https://www.pivccu.de/piVCCU stable main" >/etc/apt/sources.list.d/pivccu.list'
+    apt update
+  fi
+
+  # Install kernel headers
+  if command -v armbian-config >/dev/null; then
+    echo "Detected Armbian - install kernel sources and device tree"
+    check_sudo
+    apt install "${FORCE}" "$(dpkg --get-selections | grep 'linux-image-' | grep '\sinstall' | sed -e 's/linux-image-\([a-z0-9-]\+\).*/linux-headers-\1/')"
+    if ! pkg_installed pivccu-devicetree-armbian; then
+      check_sudo
+      apt install "${FORCE}" pivccu-devicetree-armbian
+    fi
+  elif grep -q Raspbian /etc/os-release; then
+    if ! pkg_installed pivccu-modules-raspberrypi; then
+      echo "Detected Raspbian - install kernel sources and raspberry modules"
+      check_sudo
+      apt install "${FORCE}" pivccu-modules-raspberrypi
+      echo
+      echo "NOTE: please ensure that your GPIO UART is free if you plan to connect your CCU adapter to it"
+      echo "See step 5 and 6 at https://github.com/alexreinert/piVCCU/blob/master/docs/setup/raspberrypi.md"
+    fi
+  elif ! pkg_installed "linux-headers-$(uname -r)"; then
+    echo "Generic Debian/Ubuntu platfor - trying generic way to install kernel headers"
+    check_sudo
+    apt install "${FORCE}" "linux-headers-$(uname -r)"
+  fi
+
+  # Install & Build kernel modules
+  if ! pkg_installed pivccu-modules-dkms; then
+    echo "Installing and building kernel modules..."
+    check_sudo
+    apt install "${FORCE}" pivccu-modules-dkms
+    service pivccu-dkms start
+  fi
 fi
 
-echo "Load eq3_char_loop module"
-modprobe eq3_char_loop || true
-echo eq3_char_loop>/etc/modules-load.d/eq3_char_loop.conf || true
+# setup /dev/eq3loop
+if [[ ! -c /dev/eq3loop ]]; then
+  echo "Loading eq3_char_loop module"
+  check_sudo
+  modprobe eq3_char_loop
+fi
+if [[ ! -e /etc/modules-load.d/eq3_char_loop.conf ]]; then
+  echo "Installing eq3_char_loop to /etc/modules-load.d"
+  check_sudo
+  echo eq3_char_loop >/etc/modules-load.d/eq3_char_loop.conf
+fi
 
-echo "Add udev rules"
-cat <<'EOF' > /etc/udev/rules.d/99-Homematic.rules
-  ACTION=="add", ATTRS{idVendor}=="1b1f", ATTRS{idProduct}=="c020", RUN+="/sbin/modprobe cp210x" RUN+="/bin/sh -c 'echo 1b1f c020 > /sys/bus/usb-serial/drivers/cp210x/new_id'"
+if [[ ! -e /etc/udev/rules.d/99-Homematic.rules ]]; then
+  echo "Adding udev rule"
+  check_sudo
+  cat <<'EOF' >/etc/udev/rules.d/99-Homematic.rules
+ACTION=="add", ATTRS{idVendor}=="1b1f", ATTRS{idProduct}=="c020", RUN+="/sbin/modprobe cp210x" RUN+="/bin/sh -c 'echo 1b1f c020 >/sys/bus/usb-serial/drivers/cp210x/new_id'"
+ATTRS{idVendor}=="1b1f" ATTRS{idProduct}=="c020", ENV{ID_MM_DEVICE_IGNORE}="1"
+ATTRS{idVendor}=="1b1f" ATTRS{idProduct}=="c00f", ENV{ID_MM_DEVICE_IGNORE}="1"
+ATTRS{idVendor}=="0403" ATTRS{idProduct}=="6f70", ENV{ID_MM_DEVICE_IGNORE}="1"
+ATTRS{idVendor}=="10c4" ATTRS{idProduct}=="8c07", ENV{ID_MM_DEVICE_IGNORE}="1"
 EOF
-udevadm control --reload-rules
-udevadm trigger --action=add
+  udevadm control --reload-rules
+  udevadm trigger --action=add
+fi
 
-#echo "Enable realtime"
-#echo 'kernel.sched_rt_runtime_us=-1' > /etc/sysctl.d/10-ccu.conf || true
-#sysctl -w kernel.sched_rt_runtime_us=-1 > /dev/null || true
+#############################################################
+#                      CONTAINER UPDATE                     #
+#############################################################
 
-echo "Remove old container if it exists"
-docker stop ${CCU_CONTAINER_NAME} || true
-docker rm ${CCU_CONTAINER_NAME} || true
+if docker container inspect "${CCU_CONTAINER_NAME}" >/dev/null 2>&1; then
+  echo "Stopping+Removing old container"
+  docker stop "${CCU_CONTAINER_NAME}" >/dev/null || true
+  docker rm "${CCU_CONTAINER_NAME}" >/dev/null || true
+fi
 
 DOCKER_IMAGE="${CCU_OCI_REPO}:${CCU_OCI_TAG}"
 echo "Pull/Update OCI image ${DOCKER_IMAGE}"
-docker pull ${CCU_DOCKER_PULL_OPTIONS} ${DOCKER_IMAGE}
+docker pull ${CCU_DOCKER_PULL_OPTIONS} "${DOCKER_IMAGE}"
 
-echo "Start container"
+#############################################################
+#                      DOCKER STARTUP                       #
+#############################################################
+
+echo "Starting container:"
 DOCKER_COMMAND="docker run -d -ti --privileged --restart=always -v"
-#Persistent volume
+# Persistent volume
 DOCKER_COMMAND="${DOCKER_COMMAND} ${CCU_DATA_VOLUMEN}:/usr/local"
-#Container and host names
+# Container and host names
 DOCKER_COMMAND="${DOCKER_COMMAND} --hostname ${CCU_CONTAINER_NAME} --name ${CCU_CONTAINER_NAME}"
-#Add extra ports
+# Add extra ports
 DOCKER_COMMAND="${DOCKER_COMMAND} -p ${CCU_SSH_PORT}:22 -p ${CCU_REGA_HTTP_PORT}:80 -p ${CCU_REGA_HTTPS_PORT}:443"
 for extra_port in ${CCU_PORTS_TO_OPEN}; do
   DOCKER_COMMAND="${DOCKER_COMMAND} -p ${extra_port}:${extra_port}"
 done
-#Add timeout
+# Add timeout
 DOCKER_COMMAND="${DOCKER_COMMAND} --stop-timeout ${CCU_DOCKER_STOP_TIMEOUT}"
-#Add extra user options
+# Add extra user options
 DOCKER_COMMAND="${DOCKER_COMMAND} ${CCU_DOCKER_RUN_OPTIONS}"
-#Add container repo
+# Add container repo
 DOCKER_COMMAND="${DOCKER_COMMAND} ${DOCKER_IMAGE}"
-#Execute command
-$DOCKER_COMMAND
 
-echo
-echo "Docker container started!"
-echo "- See logs with \"docker logs ${CCU_CONTAINER_NAME}\""
-echo "- Connect to http://<this server IP>:${CCU_REGA_HTTP_PORT}"
+# Execute command
+echo "${DOCKER_COMMAND}"
+if ${DOCKER_COMMAND} >/dev/null; then
+  echo
+  echo "Docker container started!"
+  echo "- See logs with \"docker logs ${CCU_CONTAINER_NAME}\""
+  echo "- Connect to http://<this server IP>:${CCU_REGA_HTTP_PORT}"
+  exit 0
+else
+  echo
+  echo "ERROR: Failed to start docker container '${CCU_CONTAINER_NAME}'"
+  exit 1
+fi

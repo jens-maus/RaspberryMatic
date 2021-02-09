04ceaaee49034d5412d34aee9226ac42934d127f
Jens Maus <mail@jens-maus.de>
2021-02-09 22:56:04 +0100

    implemented an eMMC life-time check watchdog job which will regularly check any existing '/sys/block/<diskid>/device/life_time' entry and raise a watchdog alarm in case the life time value exceeds 90% or higher. This check should come handy on systems with onboard or external eMMC storage solutions (e.g. ASUS Tinkerboard) and will regulary monitot the health status of the eMMC wear leveling.

---
+++ buildroot-external/overlay/base-raspmatic/bin/checkEMMCLifeTime.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+# shellcheck shell=dash disable=SC2169
+#
+# eMMC life time check script v1.0
+# Copyright (c) 2021 Jens Maus <mail@jens-maus.de>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# This script can be executed and will query the device where rootfs (/)
+# is mounted. It will then check if a /sys/block/<diskid>/device/life_time
+# entry exists and if so it outputs the life time level of the user
+# partition for further analysis. In addition it sets the return/exit code
+# according to the severity (0=na, 1=ok, 2=warn, 3=error)
+#
+
+ROOTDEV=$(mountpoint -n / | cut -d" " -f1)
+[[ -b ${ROOTDEV} ]] || exit 0
+
+DISKDEV=$(/bin/lsblk -s -p -d -r -n -o NAME "${ROOTDEV}" | tail -1)
+[[ -b ${DISKDEV} ]] || exit 0
+
+DISKID=$(basename "${DISKDEV}")
+[[ -n "${DISKID}" ]] || exit 0
+
+# check if /sys/block/<diskid>/device/life_time
+if [[ -f "/sys/block/${DISKID}/device/life_time" ]]; then
+  life_time_value=$(( $(cut -d' ' -f2 "/sys/block/${DISKID}/device/life_time") ))
+
+  # 0=Not defined, 1-10=0-100% device life time used, 11=Exceeded
+  if [[ ${life_time_value} -gt 0 ]]; then
+    if [[ ${life_time_value} -eq 11 ]]; then
+      echo "100%"
+      exit 3
+    elif [[ ${life_time_value} -ge 9 ]]; then
+      echo "$(( (life_time_value-1) * 10 ))-$(( life_time_value * 10 ))%"
+      exit 2
+    else
+      echo "$(( (life_time_value-1) * 10 ))-$(( life_time_value * 10 ))%"
+      exit 1
+    fi
+  fi
+fi
+
+exit 0

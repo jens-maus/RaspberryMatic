8ea6afc86005fdd0e5dfa35b48c2ec231d75f0a8
Jens Maus <mail@jens-maus.de>
2021-02-16 17:54:52 +0100

    updated generic-raw-uart package to latest master version which integrates changes for kernel 5.10 compatibility.

--- buildroot-external/package/generic_raw_uart/generic_raw_uart.mk
+++ buildroot-external/package/generic_raw_uart/generic_raw_uart.mk
@@ -7,7 +7,7 @@
 #
 #############################################################
 
-GENERIC_RAW_UART_VERSION = 752b927680adce5c1ad92d00f63f7e322ec6e51f
+GENERIC_RAW_UART_VERSION = 0fc14a2131da3b14d91407ef803ded5dc3a9ff80
 GENERIC_RAW_UART_SITE = $(call github,alexreinert,piVCCU,$(GENERIC_RAW_UART_VERSION))
 GENERIC_RAW_UART_LICENSE = GPL
 GENERIC_RAW_UART_DEPENDENCIES = host-dtc

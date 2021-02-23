bc43e72b580b9dc9936e9005d5e580f6d7e7294b
Jens Maus <mail@jens-maus.de>
2021-02-23 20:33:10 +0100

    updated OCCU to 3.57.4

--- buildroot-external/package/occu/occu.mk
+++ buildroot-external/package/occu/occu.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-OCCU_VERSION = 3.55.10-2
+OCCU_VERSION = 3.57.4-1
 OCCU_SITE = $(call github,jens-maus,occu,$(OCCU_VERSION))
 
 ifeq ($(BR2_PACKAGE_OCCU),y)

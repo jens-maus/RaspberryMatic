1c7e4d5a4beceaa39d9d6eca80313db80cb197b3
Jens Maus <mail@jens-maus.de>
2021-04-30 18:10:58 +0200

    integrated device tree overlay fixes for the odroid platform targets where the /aliases overlay didn't work correctly and thus didn't prefer the RTC of the RPI-RF-MOD in all cases.

--- buildroot-external/package/rpi-rf-mod/rpi-rf-mod.mk
+++ buildroot-external/package/rpi-rf-mod/rpi-rf-mod.mk
@@ -11,7 +11,7 @@
 #
 #############################################################
 
-RPI_RF_MOD_VERSION = 1.7.0
+RPI_RF_MOD_VERSION = 1.8.0
 RPI_RF_MOD_SITE = $(BR2_EXTERNAL_EQ3_PATH)/package/rpi-rf-mod
 RPI_RF_MOD_SITE_METHOD = local
 RPI_RF_MOD_LICENSE = Apache-2.0

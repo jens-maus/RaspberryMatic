fa5ba612e7fb0cc8f568123221837fa28deb4075
Jens Maus <mail@jens-maus.de>
2021-06-09 20:46:19 +0200

    slightly increased the minimal fan speed for >= 55 deg for the ArgonFAN to get rid of potential high frequency noises.

--- buildroot-external/package/argononed/argononed.mk
+++ buildroot-external/package/argononed/argononed.mk
@@ -3,7 +3,7 @@
 # ArgonONE/FAT GAR Support Daemon
 #
 #############################################################
-ARGONONED_VERSION = 1.0
+ARGONONED_VERSION = 1.1
 ARGONONED_SITE = $(BR2_EXTERNAL_EQ3_PATH)/package/argononed
 ARGONONED_SITE_METHOD = local
 ARGONONED_LICENSE = Apache-2.0

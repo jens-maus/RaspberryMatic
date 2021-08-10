8090fdb136375b6c4d0e8d6e6e5367468142594d
Jens Maus <mail@jens-maus.de>
2021-08-10 10:27:55 +0200

    updated Mediola NEO Server to latest 2.7.1

--- buildroot-external/package/neoserver/neoserver.mk
+++ buildroot-external/package/neoserver/neoserver.mk
@@ -3,7 +3,7 @@
 # Neo Server
 #
 #############################################################
-NEOSERVER_VERSION = 2.7.0
+NEOSERVER_VERSION = 2.7.1
 NEOSERVER_SOURCE =
 NEOSERVER_LICENSE = PROPERITARY
 

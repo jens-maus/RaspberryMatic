859964f006dc30b86494f977d11170fd64f1da58
Jérôme <jp112sdl@icloud.com>
2021-08-19 22:47:51 +0200

    added new 0131-WebUI-ShowAddonUpdatesOnStartpage WebUI patch
    Co-authored-by: Jens Maus <mail@jens-maus.de>

---
+++ buildroot-external/overlay/base-raspmatic/bin/checkAddonUpdates.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# shellcheck shell=dash disable=SC3010
+
+jsonfile=/tmp/addon_updates.json
+if [[ -n "$(ls -A /etc/config/rc.d)" ]]; then
+  echo "[" > $jsonfile
+  first=1
+  for filename in /etc/config/rc.d/*; do
+    if [[ -f $filename ]]; then
+      DINFO=$($filename info)
+      DNAME=$(echo "$DINFO" | grep "Name: " | sed "s/Name: //g")
+      DVERSION=$(echo "$DINFO" | grep "Version:" | awk '{print $2}')
+      DUPDATESCRIPT=$(echo "$DINFO" | grep "Update:" | awk '{print $2}')
+      if [[ -n "${DUPDATESCRIPT}" ]]; then
+        WEBRESULT=$(/usr/bin/curl -s "http://localhost${DUPDATESCRIPT}")
+        if [[ "$first" -eq 1 ]]; then
+          first=0
+        else
+          echo "," >> $jsonfile
+        fi
+        echo "{\"name\":\"$DNAME\",\"webversion\":\"$WEBRESULT\"}" >> $jsonfile
+        if [[ "$DVERSION" != "$WEBRESULT" ]]; then
+          echo "Update available for $DNAME ($DVERSION / $WEBRESULT)"
+        fi
+      fi
+    fi
+  done
+  echo "]" >> $jsonfile
+else
+  [[ -f $jsonfile ]] && rm $jsonfile
+fi

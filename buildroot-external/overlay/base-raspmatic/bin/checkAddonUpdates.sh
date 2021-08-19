f7bc96569d440771824a9606e1b6281847e949d2
jp112sdl <jp112sdl@icloud.com>
2021-08-19 10:01:07 +0200

    add patch for #1384

---
+++ buildroot-external/overlay/base-raspmatic/bin/checkAddonUpdates.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+jsonfile=/tmp/addon_updates.json
+if [ ! -z "$(ls -A /etc/config/rc.d)" ]; then
+  echo "[" > $jsonfile
+  first=1
+  for filename in /etc/config/rc.d/*; do
+    if [ -f $filename ]; then
+      DINFO=`$filename info`
+      DNAME=`echo "$DINFO" | grep "Name: "|sed "s/Name: //g"`
+      DVERSION=`echo "$DINFO" | grep "Version:"|awk '{print $2}'`
+      DUPDATESCRIPT=`echo "$DINFO" | grep "Update:"|awk '{print $2}'`
+      if [ ! -z $DUPDATESCRIPT ]; then
+        QUERY_STRING='cmd=check_version\&version=$DVERSION'
+        WEBRESULT=`export QUERY_STRING=$QUERY_STRING;tclsh /www${DUPDATESCRIPT}|grep "\."` 
+        if [ "$first" -eq 1 ]; then 
+          first=0
+        else
+          echo "," >> $jsonfile
+        fi
+        echo "{\"name\":\"$DNAME\",\"webversion\":\"$WEBRESULT\"}" >> $jsonfile
+        if [ "$DVERSION" != "$WEBRESULT" ]; then
+          echo "Update available for $DNAME ($DVERSION / $WEBRESULT)"
+        fi
+      fi
+    fi
+  done
+  echo "]" >> $jsonfile
+else
+  [[ -f $jsonfile ]] && rm $jsonfile
+fi

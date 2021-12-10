1f844288d78b561a7226fe7207475976aea20b04
Jens Maus <mail@jens-maus.de>
2021-12-10 16:53:47 +0100

    updated detect_radio_module package to latest version available.

--- buildroot-external/package/detect_radio_module/detect_radio_module.mk
+++ buildroot-external/package/detect_radio_module/detect_radio_module.mk
@@ -7,7 +7,7 @@
 #
 ################################################################################
 
-DETECT_RADIO_MODULE_VERSION = d1dddbeb32657da72845edd0e4a097fbce3a0ec6
+DETECT_RADIO_MODULE_VERSION = c26d8a4cc141cdc65ff9d01a135ac0b527584234
 DETECT_RADIO_MODULE_SITE = $(call github,alexreinert,piVCCU,$(DETECT_RADIO_MODULE_VERSION))
 DETECT_RADIO_MODULE_LICENSE = Apache-2.0
 DETECT_RADIO_MODULE_LICENSE_FILES = LICENSE

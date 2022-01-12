c931456b0734fe1753129c52abe7dbc72fb7dea7
Jens Maus <mail@jens-maus.de>
2022-01-12 13:01:01 +0100

    updated OCCU to latest 3.61.7-3 which comes with ReGaHss R1.00.0388.0229 which in turn fixes #1643.

--- buildroot-external/package/occu/occu.mk
+++ buildroot-external/package/occu/occu.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OCCU_VERSION = 3.61.7-2
+OCCU_VERSION = 3.61.7-3
 OCCU_SITE = $(call github,jens-maus,occu,$(OCCU_VERSION))
 OCCU_LICENSE = HMSL
 OCCU_LICENSE_FILES = LicenseDE.txt

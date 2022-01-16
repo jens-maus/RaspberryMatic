08bb7b2b6e07165a05a5811894f46be9856d7844
Jens Maus <mail@jens-maus.de>
2022-01-16 22:45:41 +0100

    added a first basic version of an lxc platform integration to run RaspberryMatic within a LXC container environment.

---
+++ buildroot-external/board/lxc/post-image.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Stop on error
+set -e
+
+BOARD_DIR="$(dirname "$0")"
+BOARD_NAME="$(basename "${BOARD_DIR}")"
+
+# make sure a factory reset is performed upon fresh start
+touch "${TARGET_DIR}/usr/local/.doFactoryReset"
+
+exit $?
---
+++ buildroot-external/board/lxc/post-release.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Stop on error
+set -e
+
+#BOARD_DIR=${1}
+PRODUCT=${2}
+PRODUCT_VERSION=${3}
+BOARD=$(echo "${PRODUCT}" | cut -d'_' -f2-)
+
+# copy prepared docker image to release path
+cp -a "build-${PRODUCT}/images/RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tar" ./release/
+#(cd "./release/updatepkg/${PRODUCT}" && tar --numeric-owner --owner=root --group=root -rvf "../../RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tar" update_script EULA.de EULA.en)
+gzip -f "./release/RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tar"
+mv "./release/RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tar.gz" "./release/RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tgz"
+(cd ./release && sha256sum "RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tgz" >"RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tgz.sha256")
+
+exit $?

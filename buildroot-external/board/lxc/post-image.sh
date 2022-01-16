def9e8fecba793d7985c9064ce75c0d8713a65bf
Jens Maus <mail@jens-maus.de>
2022-01-16 23:50:04 +0100

    fixed linter error

--- buildroot-external/board/lxc/post-image.sh
+++ buildroot-external/board/lxc/post-image.sh
@@ -3,8 +3,8 @@
 # Stop on error
 set -e
 
-BOARD_DIR="$(dirname "$0")"
-BOARD_NAME="$(basename "${BOARD_DIR}")"
+#BOARD_DIR="$(dirname "$0")"
+#BOARD_NAME="$(basename "${BOARD_DIR}")"
 
 # make sure a factory reset is performed upon fresh start
 touch "${TARGET_DIR}/usr/local/.doFactoryReset"

039bdbedf56583e5df33a618b74edb22644a4325
Jens Maus <mail@jens-maus.de>
2022-03-17 12:34:32 +0100

    updated rpi4 rpi-eeprom to latest pieeprom-2022-03-10.bin

--- buildroot-external/package/rpi-eeprom/rpi-eeprom.mk
+++ buildroot-external/package/rpi-eeprom/rpi-eeprom.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-RPI_EEPROM_VERSION = eb4502d528d889917e6b321969deb9d216ddeedc
+RPI_EEPROM_VERSION = e8927007e3800db3a72100ee6cd38b0d9b687c16
 RPI_EEPROM_SITE = $(call github,raspberrypi,rpi-eeprom,$(RPI_EEPROM_VERSION))
 RPI_EEPROM_LICENSE = BSD-3-Clause
 RPI_EEPROM_LICENSE_FILES = LICENSE
 RPI_EEPROM_INSTALL_IMAGES = YES
-RPI_EEPROM_FIRMWARE_PATH = firmware/stable/pieeprom-2022-02-08.bin
+RPI_EEPROM_FIRMWARE_PATH = firmware/stable/pieeprom-2022-03-10.bin
 
 define RPI_EEPROM_BUILD_CMDS
 	$(@D)/rpi-eeprom-config $(@D)/$(RPI_EEPROM_FIRMWARE_PATH) --out $(@D)/default.conf

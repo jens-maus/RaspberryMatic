8bac84398de5dc772b4676ecabc498e2cd2edd5e
Jens Maus <mail@jens-maus.de>
2022-05-22 11:46:39 +0200

    create tar.gz instead.

--- buildroot-external/board/lxc/post-release.sh
+++ buildroot-external/board/lxc/post-release.sh
@@ -9,10 +9,9 @@ PRODUCT_VERSION=${3}
 BOARD=$(echo "${PRODUCT}" | cut -d'_' -f2-)
 
 # copy prepared docker image to release path
-cp -a "build-${PRODUCT}/images/RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tar" ./release/
+cp -a "build-${PRODUCT}/images/rootfs.tar" "./release/RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tar"
 #(cd "./release/updatepkg/${PRODUCT}" && tar --numeric-owner --owner=root --group=root -rvf "../../RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tar" update_script EULA.de EULA.en)
 gzip -f "./release/RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tar"
-mv "./release/RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tar.gz" "./release/RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tgz"
-(cd ./release && sha256sum "RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tgz" >"RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tgz.sha256")
+(cd ./release && sha256sum "RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tar.gz" >"RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tar.gz.sha256")
 
 exit $?

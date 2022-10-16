d242973fe81c315d7d412108a490b07bc0d87118
Jens Maus <mail@jens-maus.de>
2022-10-16 16:38:23 +0200

    added a first "generic-arm64" platform prototype. This refs #1937.

---
+++ buildroot-external/board/generic-arm64/post-build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Stop on error
+set -e
+
+# copy the kernel image to rootfs
+cp -a "${BINARIES_DIR}/bzImage" "${TARGET_DIR}/zImage"
+
+# create VERSION file
+echo "VERSION=${PRODUCT_VERSION}" >"${TARGET_DIR}/VERSION"
+echo "PRODUCT=${PRODUCT}" >>"${TARGET_DIR}/VERSION"
+echo "PLATFORM=generic-arm64" >>"${TARGET_DIR}/VERSION"
+
+# fix some permissions
+[ -e "${TARGET_DIR}/etc/monitrc" ] && chmod 600 "${TARGET_DIR}/etc/monitrc"
+
+# copy grub boot.img to binaries dir
+cp -f "${HOST_DIR}/../build/grub2-2.06/build-i386-pc/grub-core/boot.img" "${BINARIES_DIR}/"
+
+# link VERSION in /boot on rootfs
+mkdir -p "${TARGET_DIR}/boot"
+ln -sf ../VERSION "${TARGET_DIR}/boot/VERSION"

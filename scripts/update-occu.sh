e76a43e7a86914cf2a1de81a2ce59d94071c1d0a
Jens Maus <mail@jens-maus.de>
2023-01-27 13:37:39 +0100

    introduce automatic hash update.

--- scripts/update-occu.sh
+++ scripts/update-occu.sh
@@ -6,4 +6,15 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-sed -i "s/OCCU_VERSION = .*/OCCU_VERSION = $1/g" buildroot-external/package/occu/occu.mk
+OCCU_VERSION=$1
+DOWNLOAD_URL="https://github.com/jens-maus/occu/archive/${OCCU_VERSION}/occu-${OCCU_VERSION}.tar.gz"
+
+# download archive for hash update
+HASH=$(wget --passive-ftp -nd -t 3 -O - ${DOWNLOAD_URL} | sha256sum | awk '{ print $1 }')
+if [ -n "${HASH}" ]; then
+  # update package info
+  sed -i "s/OCCU_VERSION = .*/OCCU_VERSION = $1/g" buildroot-external/package/occu/occu.mk
+  # update package hash
+  sed -i "$ d" buildroot-external/package/occu/occu.hash
+  echo "sha256  ${HASH}  occu-${OCCU_VERSION}.tar.gz" >> buildroot-external/package/occu/occu.hash
+fi

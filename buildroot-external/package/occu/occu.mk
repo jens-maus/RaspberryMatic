7f8dde2ded37ed12d8a2fe822cb60af70644dfec
Jens Maus <mail@jens-maus.de>
2023-01-27 13:35:59 +0100

    updated occu to 3.67.10-4 which comes with ReGaHss R1.00.0388.0233 which introduces another related fix for #2008.

--- buildroot-external/package/occu/occu.mk
+++ buildroot-external/package/occu/occu.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OCCU_VERSION = 3.67.10-3
+OCCU_VERSION = 3.67.10-4
 OCCU_SITE = $(call github,jens-maus,occu,$(OCCU_VERSION))
 OCCU_LICENSE = HMSL
 OCCU_LICENSE_FILES = LicenseDE.txt

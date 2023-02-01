d937d1c8dd8f09456c4a2c197758920639eab9d1
Jens Maus <mail@jens-maus.de>
2023-02-01 14:58:03 +0100

    disable SC2086 for the next line to get the docker pull command running correctly.

--- buildroot-external/board/oci/deploy.sh
+++ buildroot-external/board/oci/deploy.sh
@@ -177,6 +177,7 @@ fi
 DOCKER_IMAGE="${CCU_OCI_REPO}:${CCU_OCI_TAG}"
 if [[ "${CCU_DOCKER_PULL_REFRESH}" == "true" ]]; then
   echo "Pull/Update OCI image ${DOCKER_IMAGE}"
+  # shellcheck disable=SC2086
   docker pull ${CCU_DOCKER_PULL_OPTIONS} "${DOCKER_IMAGE}"
 fi
 

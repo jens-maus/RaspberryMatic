f872a0bef29cbc25dc50f4a5475f80e7757f4902
Jens Maus <mail@jens-maus.de>
2023-08-17 23:19:06 +0200

    updated tailscale to 1.46.1

--- buildroot-external/package/tailscale/tailscale.mk
+++ buildroot-external/package/tailscale/tailscale.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-TAILSCALE_VERSION = 1.44.0
+TAILSCALE_VERSION = 1.46.1
 TAILSCALE_SITE = https://pkgs.tailscale.com/stable
 ifeq ($(call qstrip,$(BR2_ARCH)),arm)
 TAILSCALE_SOURCE = tailscale_$(TAILSCALE_VERSION)_arm.tgz

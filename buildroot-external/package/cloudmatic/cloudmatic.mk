2a843e14b48f3ae0f843a092a515e36e4a781346
Jens Maus <mail@jens-maus.de>
2023-10-11 18:58:17 +0200

    updated CloudMatic to commit with integrated raspberrymatic pull request (this refs #2442)

--- buildroot-external/package/cloudmatic/cloudmatic.mk
+++ buildroot-external/package/cloudmatic/cloudmatic.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CLOUDMATIC_VERSION = 98d4105883b785305e6c54b857e0143a1385ba3c
+CLOUDMATIC_VERSION = 021193b439095beb1758588456660a480f02b2f1
 CLOUDMATIC_SITE = $(call github,EasySmartHome,CloudMatic-CCUAddon,$(CLOUDMATIC_VERSION))
 CLOUDMATIC_LICENSE = BSD-3-Clause
 

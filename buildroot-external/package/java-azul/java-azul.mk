1bf3ea3a5ecc45d4065ddb21b05f9a4a170b7737
Jens Maus <mail@jens-maus.de>
2023-11-06 21:50:27 +0100

    updated java azul version to Java11 as the HMIPServer of OCCU 3.73.x is compiled using Java11 itself.

--- buildroot-external/package/java-azul/java-azul.mk
+++ buildroot-external/package/java-azul/java-azul.mk
@@ -4,8 +4,9 @@
 #
 ################################################################################
 
-JAVA_AZUL_VERSION = 8.72.0.17-ca-jre8.0.382
+JAVA_AZUL_VERSION = 11.68.17-ca-jre11.0.21
 ifeq ($(call qstrip,$(BR2_ARCH)),arm)
+JAVA_AZUL_VERSION = 11.66.19-ca-jre11.0.20.1
 JAVA_AZUL_SOURCE = zulu$(JAVA_AZUL_VERSION)-linux_aarch32hf.tar.gz
 JAVA_AZUL_SITE = https://cdn.azul.com/zulu-embedded/bin
 else ifeq ($(call qstrip,$(BR2_ARCH)),aarch64)
@@ -25,9 +26,9 @@ JAVA_AZUL_DEPENDENCIES = fontconfig dejavu liberation
 define JAVA_AZUL_INSTALL_TARGET_CMDS
 	$(INSTALL) -d -m 0755 $(TARGET_DIR)/opt/java-azul
 	cp -a $(@D)/bin $(TARGET_DIR)/opt/java-azul/
-	#cp -a $(@D)/conf $(TARGET_DIR)/opt/java-azul/
+	cp -a $(@D)/conf $(TARGET_DIR)/opt/java-azul/
 	cp -a $(@D)/lib $(TARGET_DIR)/opt/java-azul/
-	#cp -a $(@D)/legal $(TARGET_DIR)/opt/java-azul/
+	cp -a $(@D)/legal $(TARGET_DIR)/opt/java-azul/
 	cp -a $(@D)/DISCLAIMER $(TARGET_DIR)/opt/java-azul/
 	rm -f $(TARGET_DIR)/opt/java
 	ln -s java-azul $(TARGET_DIR)/opt/java

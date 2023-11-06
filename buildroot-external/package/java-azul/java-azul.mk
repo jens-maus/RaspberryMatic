e837422100005fe512ec3b6d759d3e8ca43db0e9
Jens Maus <mail@jens-maus.de>
2023-11-06 23:12:08 +0100

    fix java azul site url for aarch64

--- buildroot-external/package/java-azul/java-azul.mk
+++ buildroot-external/package/java-azul/java-azul.mk
@@ -11,7 +11,7 @@ JAVA_AZUL_SOURCE = zulu$(JAVA_AZUL_VERSION)-linux_aarch32hf.tar.gz
 JAVA_AZUL_SITE = https://cdn.azul.com/zulu-embedded/bin
 else ifeq ($(call qstrip,$(BR2_ARCH)),aarch64)
 JAVA_AZUL_SOURCE = zulu$(JAVA_AZUL_VERSION)-linux_aarch64.tar.gz
-JAVA_AZUL_SITE = https://cdn.azul.com/zulu-embedded/bin
+JAVA_AZUL_SITE = https://cdn.azul.com/zulu/bin
 else ifeq ($(call qstrip,$(BR2_ARCH)),i686)
 JAVA_AZUL_SOURCE = zulu$(JAVA_AZUL_VERSION)-linux_i686.tar.gz
 JAVA_AZUL_SITE = https://cdn.azul.com/zulu/bin

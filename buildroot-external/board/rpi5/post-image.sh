bdee2b9eda6857c8779efd5cafd4eafe816d5300
Jens Maus <mail@jens-maus.de>
2023-11-13 11:02:40 +0100

    WIP: Raspberry Pi 5 support
    This commit implements a first basicly working Raspberry Pi 5 support
    which is able to boot into the main RaspberryMatic system and seems to
    work as expected with the following exceptions/todo:
    
    1. No U-boot bootloader: due to no bcm2712/pi5 support in the upstream
       U-Boot sources, there is currently also no u-boot support like with
       the other rpi platforms/boards. That means, there is currently no
       possibility to boot into the recovery system, thus no way to update
       this system via standard update packages/procedures.
    2. No framebuffer console: due to the missing u-boot support and also
       potentially due to the stripped down kernel configs there is
       currently no framebuffer console available for direct login via a HDMI
       connected monitor.
    3. High carrier sense with RPI-RF-MOD@GPIO: Unfortunately, it
       seems that a RPI-RF-MOD rf module directly connected to the GPIO
       of a Raspberry Pi 5 does exhibit quite a high CarrierSense (> 80%)
       due to massive RF interferences. More analyses and work seems to be
       required to try to reduce this, thought.

---
+++ buildroot-external/board/rpi5/post-image.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Stop on error
+set -e
+
+BOARD_DIR="$(dirname "$0")"
+BOARD_NAME="$(basename "${BOARD_DIR}")"
+
+# Use our own cmdline.txt+config.txt
+cp "${BR2_EXTERNAL_EQ3_PATH}/board/${BOARD_NAME}/cmdline.txt" "${BINARIES_DIR}/"
+cp "${BR2_EXTERNAL_EQ3_PATH}/board/${BOARD_NAME}/config.txt" "${BINARIES_DIR}/"
+
+#
+# Create user filesystem
+#
+echo "Create user filesystem"
+mkdir -p "${BUILD_DIR}/userfs"
+touch "${BUILD_DIR}/userfs/.doFactoryReset"
+rm -f "${BINARIES_DIR}/userfs.ext4"
+"${HOST_DIR}/sbin/mkfs.ext4" -d "${BUILD_DIR}/userfs" -F -L userfs -I 256 -E lazy_itable_init=0,lazy_journal_init=0 "${BINARIES_DIR}/userfs.ext4" 3000
+
+#
+# VERSION File
+#
+cp "${TARGET_DIR}/boot/VERSION" "${BINARIES_DIR}"
+
+# create *.img file using genimage
+support/scripts/genimage.sh -c "${BR2_EXTERNAL_EQ3_PATH}/board/${BOARD_NAME}/genimage.cfg"

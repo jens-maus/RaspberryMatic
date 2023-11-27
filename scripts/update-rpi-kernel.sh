8ecf18d4c02c7ef1997d8804ad9e0e39112c4d04
Jens Maus <mail@jens-maus.de>
2023-11-27 21:25:44 +0100

    adapted update-rpi-kernel.sh to also update hash files accordingly.

--- scripts/update-rpi-kernel.sh
+++ scripts/update-rpi-kernel.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 set -e
 
-if [ -z "$1" ]; then
-    echo "Need a tag or commit ID!"
-    exit 1
+ID=${1}
+PACKAGE_NAME="linux"
+PROJECT_URL="https://github.com/raspberrypi/linux"
+ARCHIVE_URL="${PROJECT_URL}/archive/${ID}/${ID}.tar.gz"
+
+if [[ -z "${ID}" ]]; then
+  echo "tag name or commit sha required (see ${URL})"
+  exit 1
 fi
 
-sed -i "s|BR2_LINUX_KERNEL_CUSTOM_TARBALL_LOCATION=\"https://github.com/raspberrypi/linux/.*\"|BR2_LINUX_KERNEL_CUSTOM_TARBALL_LOCATION=\"https://github.com/raspberrypi/linux/archive/$1.tar.gz\"|g" buildroot-external/configs/raspmatic_rpi*
-#git commit -m "RaspberryPi: Update kernel $2 - $1" buildroot-external/configs/* Documentation/kernel.md
+# download archive for hash update
+ARCHIVE_HASH=$(wget --passive-ftp -nd -t 3 -O - "${ARCHIVE_URL}" | sha256sum | awk '{ print $1 }')
+if [[ -n "${ARCHIVE_HASH}" ]]; then
+
+  # get old stable version
+  OLD_PACKAGE=$(sed -n 's/BR2_LINUX_KERNEL_CUSTOM_TARBALL_LOCATION="\(.*\)"/\1/p' buildroot-external/configs/raspmatic_rpi* | head -1 | xargs basename)
+
+  # update package info
+  sed -i "s|BR2_LINUX_KERNEL_CUSTOM_TARBALL_LOCATION=\"${PROJECT_URL}/.*\"|BR2_LINUX_KERNEL_CUSTOM_TARBALL_LOCATION=\"${PROJECT_URL}/archive/${ID}.tar.gz\"|g" buildroot-external/configs/raspmatic_rpi*
+
+  # update hash files
+  sed -i "/${OLD_PACKAGE}/d" "buildroot-external/patches/${PACKAGE_NAME}/${PACKAGE_NAME}.hash"
+  echo "sha256  ${ARCHIVE_HASH}  ${ID}.tar.gz" >>"buildroot-external/patches/${PACKAGE_NAME}/${PACKAGE_NAME}.hash"
+  sed -i "/${OLD_PACKAGE}/d" "buildroot-external/patches/${PACKAGE_NAME}-headers/${PACKAGE_NAME}-headers.hash"
+  echo "sha256  ${ARCHIVE_HASH}  ${ID}.tar.gz" >>"buildroot-external/patches/${PACKAGE_NAME}-headers/${PACKAGE_NAME}-headers.hash"
+fi

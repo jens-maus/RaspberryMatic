2bfbaf1a6517a866a42940ddb193aa303eb49f09
Jens Maus <mail@jens-maus.de>
2024-02-02 15:55:53 +0100

    add LXC container platform support for amd64, arm64 and arm(#2636)

---
+++ buildroot-external/board/lxc/post-release.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Stop on error
+set -e
+
+#BOARD_DIR=${1}
+PRODUCT=${2}
+PRODUCT_VERSION=${3}
+BOARD=$(echo "${PRODUCT}" | cut -d'_' -f2-)
+
+# copy prepared docker image to release path
+cp -a "build-${PRODUCT}/images/rootfs.tar" "./release/RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tar"
+#(cd "./release/updatepkg/${PRODUCT}" && tar --numeric-owner --owner=root --group=root -rvf "../../RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tar" update_script EULA.de EULA.en)
+gzip -f "./release/RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tar"
+(cd ./release && sha256sum "RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tar.gz" >"RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tar.gz.sha256")
+
+# create manifest file with checksum+sizes
+rm -f "./release/RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.mf"
+
+# shellcheck disable=SC2129
+echo "$(stat -c %s "./release/RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tar.gz") $(cat "./release/RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tar.gz.sha256")" >>"./release/RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.mf"

3d25ae83c18a1d142e6a5e98aed6ecb2816548c1
Jens Maus <mail@jens-maus.de>
2024-02-02 10:52:02 +0100

    prefer tar.gz as file extension for lxc packages

--- buildroot-external/board/lxc/post-release.sh
+++ buildroot-external/board/lxc/post-release.sh
@@ -12,11 +12,10 @@ BOARD=$(echo "${PRODUCT}" | cut -d'_' -f2-)
 cp -a "build-${PRODUCT}/images/rootfs.tar" "./release/RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tar"
 #(cd "./release/updatepkg/${PRODUCT}" && tar --numeric-owner --owner=root --group=root -rvf "../../RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tar" update_script EULA.de EULA.en)
 gzip -f "./release/RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tar"
-mv "./release/RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tar.gz" "./release/RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tgz"
-(cd ./release && sha256sum "RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tgz" >"RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tgz.sha256")
+(cd ./release && sha256sum "RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tar.gz" >"RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tar.gz.sha256")
 
 # create manifest file with checksum+sizes
 rm -f "./release/RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.mf"
 
 # shellcheck disable=SC2129
-echo "$(stat -c %s "./release/RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tgz") $(cat "./release/RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tgz.sha256")" >>"./release/RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.mf"
+echo "$(stat -c %s "./release/RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tar.gz") $(cat "./release/RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.tar.gz.sha256")" >>"./release/RaspberryMatic-${PRODUCT_VERSION}-${BOARD}.mf"

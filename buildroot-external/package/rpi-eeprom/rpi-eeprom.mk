ed29d0f9fc3ed336f35e03b41d5303a8fda7fcda
Jens Maus <mail@jens-maus.de>
2024-04-07 16:48:23 +0200

    bump rpi-eeprom to latest pieeprom-2024-04-05.bin for Pi5.

--- buildroot-external/package/rpi-eeprom/rpi-eeprom.mk
+++ buildroot-external/package/rpi-eeprom/rpi-eeprom.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_EEPROM_VERSION = b745226b41ac202976ee8307fcb179a1193fab3c
+RPI_EEPROM_VERSION = 61023cbd32725a07e094f9b2d01df302f4ddabba
 RPI_EEPROM_SITE = $(call github,raspberrypi,rpi-eeprom,$(RPI_EEPROM_VERSION))
 RPI_EEPROM_LICENSE = BSD-3-Clause
 RPI_EEPROM_LICENSE_FILES = LICENSE
@@ -15,7 +15,7 @@ ifeq ($(BR2_PACKAGE_RPI_EEPROM_RPI4),y)
   RPI_EEPROM_FIRMWARE_PATH = firmware-2711/stable/pieeprom-2024-01-22.bin
 else ifeq ($(BR2_PACKAGE_RPI_EEPROM_RPI5),y)
   # Raspberry Pi 5
-  RPI_EEPROM_FIRMWARE_PATH = firmware-2712/stable/pieeprom-2024-02-16.bin
+  RPI_EEPROM_FIRMWARE_PATH = firmware-2712/stable/pieeprom-2024-04-05.bin
 endif
 
 define RPI_EEPROM_BUILD_CMDS

175114aa939855e6ca749b8a00782b49193de3ca
Jens Maus <mail@jens-maus.de>
2024-04-18 09:27:58 +0200

    update integrated Mediola NEOserver to 2.15.0 and adapted update script.

--- buildroot-external/package/neoserver/neoserver.mk
+++ buildroot-external/package/neoserver/neoserver.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NEOSERVER_VERSION = 2.14.1
+NEOSERVER_VERSION = 2.15.0
 NEOSERVER_SOURCE = neo_server.tar.gz
 NEOSERVER_SITE = $(BR2_EXTERNAL_EQ3_PATH)/package/neoserver
 NEOSERVER_SITE_METHOD = file
--- scripts/update-neoserver.sh
+++ scripts/update-neoserver.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 set -e
 
-if [ -z "$1" ]; then
-  echo "Need a NEOSERVER version number (see https://mediola.de)"
-  exit 1
-fi
+PACKAGE_NAME="neoserver"
+DOWNLOAD_URL="https://s3-eu-west-1.amazonaws.com/mediola-download/ccu3/neo_server.tar.gz"
+VERSION_URL="https://s3-eu-west-1.amazonaws.com/mediola-download/ccu3/VERSION"
 
-sed -i "s/NEOSERVER_VERSION = .*/NEOSERVER_VERSION = $1/g" buildroot-external/package/neoserver/neoserver.mk
+# download/check current version
+VERSION=$(curl ${VERSION_URL})
+
+# download latest archive
+wget --passive-ftp -nd -t 3 -O "buildroot-external/package/${PACKAGE_NAME}/neo_server.tar.gz" ${DOWNLOAD_URL}
+
+# update package info
+BR_PACKAGE_NAME=${PACKAGE_NAME^^}
+BR_PACKAGE_NAME=${BR_PACKAGE_NAME//-/_}
+sed -i "s/${BR_PACKAGE_NAME}_VERSION = .*/${BR_PACKAGE_NAME}_VERSION = ${VERSION}/g" "buildroot-external/package/${PACKAGE_NAME}/${PACKAGE_NAME}.mk"
+
+# update package hash
+ARCHIVE_HASH=$(cat "buildroot-external/package/${PACKAGE_NAME}/neo_server.tar.gz" | sha256sum | awk '{ print $1 }')
+if [[ -n "${ARCHIVE_HASH}" ]]; then
+  sed -i "/neo_server\.tar\.gz/d" "buildroot-external/package/${PACKAGE_NAME}/${PACKAGE_NAME}.hash"
+  echo "sha256  ${ARCHIVE_HASH}  neo_server.tar.gz" >>"buildroot-external/package/${PACKAGE_NAME}/${PACKAGE_NAME}.hash"
+fi

6afa5c494acd422846455439f819ac878d09b246
Jens Maus <mail@jens-maus.de>
2024-04-22 21:01:19 +0200

    update wiringpi-odroid package to latest master branch version.

--- buildroot-external/package/wiringpi-odroid/wiringpi-odroid.mk
+++ buildroot-external/package/wiringpi-odroid/wiringpi-odroid.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WIRINGPI_ODROID_VERSION = fb3fe0a2a74c5a2dc97185d253d2c667a6344cc9
+WIRINGPI_ODROID_VERSION = f3f2a2ff90a3356322b5c5b40aeca9579ad3393a
 WIRINGPI_ODROID_SITE = $(call github,hardkernel,wiringPi,$(WIRINGPI_ODROID_VERSION))
 
 WIRINGPI_ODROID_LICENSE = LGPL-3.0+

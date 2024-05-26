f940f863c8ac6033b284ae2746a4a8134eff4196
Jens Maus <mail@jens-maus.de>
2024-05-26 20:31:29 +0200

    change to use GPT partition tables and EFI bootup for generic-x86_64

--- buildroot-external/board/generic-x86_64/post-build.sh
+++ buildroot-external/board/generic-x86_64/post-build.sh
@@ -14,9 +14,6 @@ echo "PLATFORM=generic-x86_64" >>"${TARGET_DIR}/VERSION"
 # fix some permissions
 [ -e "${TARGET_DIR}/etc/monitrc" ] && chmod 600 "${TARGET_DIR}/etc/monitrc"
 
-# copy grub boot.img to binaries dir
-cp -f "${HOST_DIR}"/../build/grub2-2.??/build-i386-pc/grub-core/boot.img "${BINARIES_DIR}/"
-
 # link VERSION in /boot on rootfs
 mkdir -p "${TARGET_DIR}/boot"
 ln -sf ../VERSION "${TARGET_DIR}/boot/VERSION"

2c70dcb3a546fed8a8738a06ed8bf4e9971eb7b8
Jens Maus <mail@jens-maus.de>
2024-06-01 19:26:44 +0200

    rename the "intelnuc" platform to "generic-x86_64" and use (U)EFI instead (#2759)
    This change renames the intelnuc platform to generic-x86_64 to signal its more general nature to a generic x86_64 drives platform implementation for RaspberryMatic. In addition, it also changes from Legacy Boot/BIOS to (U)EFI boot similar to generic-arm64 already is which should make this platform more future proof for hardware that boots only (U)EFI.

--- buildroot-external/board/generic-x86_64/post-build.sh
+++ buildroot-external/board/generic-x86_64/post-build.sh
@@ -9,13 +9,10 @@ cp -a "${BINARIES_DIR}/bzImage" "${TARGET_DIR}/zImage"
 # create VERSION file
 echo "VERSION=${PRODUCT_VERSION}" >"${TARGET_DIR}/VERSION"
 echo "PRODUCT=${PRODUCT}" >>"${TARGET_DIR}/VERSION"
-echo "PLATFORM=intelnuc" >>"${TARGET_DIR}/VERSION"
+echo "PLATFORM=generic-x86_64" >>"${TARGET_DIR}/VERSION"
 
 # fix some permissions
 [ -e "${TARGET_DIR}/etc/monitrc" ] && chmod 600 "${TARGET_DIR}/etc/monitrc"
-
-# copy grub boot.img to binaries dir
-cp -f "${HOST_DIR}"/../build/grub2-2.??/build-i386-pc/grub-core/boot.img "${BINARIES_DIR}/"
 
 # link VERSION in /boot on rootfs
 mkdir -p "${TARGET_DIR}/boot"

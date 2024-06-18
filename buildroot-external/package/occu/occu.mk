0864b00c9c66f0caa10b003894789c7d6768f465
Jens Maus <mail@jens-maus.de>
2024-06-18 15:51:38 +0200

    update OCCU to latest 3.77.4-1 (refs #2773)

--- buildroot-external/package/occu/occu.mk
+++ buildroot-external/package/occu/occu.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OCCU_VERSION = 3.77.2-1
+OCCU_VERSION = 3.77.4-1
 OCCU_SITE = $(call github,jens-maus,occu,$(OCCU_VERSION))
 OCCU_LICENSE = HMSL
 OCCU_LICENSE_FILES = LicenseDE.txt

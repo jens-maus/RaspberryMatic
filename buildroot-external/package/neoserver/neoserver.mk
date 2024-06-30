9b1ce3eda801021a1e0ced4bd622f9e66c03278c
Jens Maus <mail@jens-maus.de>
2024-06-30 13:59:14 +0200

    update Mediola NEOserver to latest 2.15.2

--- buildroot-external/package/neoserver/neoserver.mk
+++ buildroot-external/package/neoserver/neoserver.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NEOSERVER_VERSION = 2.15.1
+NEOSERVER_VERSION = 2.15.2
 NEOSERVER_SOURCE = neo_server.tar.gz
 NEOSERVER_SITE = $(BR2_EXTERNAL_EQ3_PATH)/package/neoserver
 NEOSERVER_SITE_METHOD = file

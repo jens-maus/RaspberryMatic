acf21d10c768c9d4636670a3dfeabccca38e454a
Jens Maus <mail@jens-maus.de>
2024-08-22 19:34:12 +0200

    implemented possibility to call script in non-interactive mode via a new "-n" option and added short documentation on how to override certain settings when calling the script.

--- scripts/patch-ha-addon-macvlan.sh
+++ scripts/patch-ha-addon-macvlan.sh
@@ -4,13 +4,53 @@
 # the raspberrymatic will be additionally connected via a macvlan interface
 # to be able to connect a HmIP-HAP/HmIPW-DRAP.
 #
+# Can be called in full interactive mode by just calling it via:
+#
+# ./patch-ha-addon-macvlan.sh
+#
+# or via overriding certain settings like:
+#
+# CCU_NETWORK_INTERFACE=eth1 CCU_CONTAINER_IP=192.168.178.20 ./patch-ha-addon-macvlan.sh
+#
+# or in full non-interactive mode accepting all defaults with
+# allowing to partly override certain defaults:
+#
+# CCU_CONTAINER_IP=192.168.178.30 ./patch-ha-addon-macvlan.sh -n
+#
 # Copyright (c) 2023-2024 Jens Maus <mail@jens-maus.de>
 # Apache 2.0 License applies
 #
 
-echo "RaspberryMatic HA-Addon macvlan patch script v1.1"
+#############################################################
+#                           Settings                        #
+#############################################################
+
+# network interface (e.g. 'eth0')
+: "${CCU_NETWORK_INTERFACE:=""}"
+
+# network subnet (e.g. '192.168.178.0/24')
+: "${CCU_NETWORK_SUBNET:=""}"
+
+# network gateway (e.g. '192.168.178.1')
+: "${CCU_NETWORK_GATEWAY:=""}"
+
+# raspberrymatic container name (e.g. '5422eb72-raspberrymatic')
+: "${CCU_CONTAINER_NAME:=""}"
+
+# raspberrymatic container ip (e.g. '192.168.178.4')
+: "${CCU_CONTAINER_IP:=""}"
+
+#############################################################
+#                         Main App                          #
+#############################################################
+echo "RaspberryMatic HA-Addon macvlan patch script v1.2"
 echo "Copyright (c) 2023-2024 Jens Maus <mail@jens-maus.de>"
 echo
+
+# check if non-interactive mode is requested
+if [[ "${1}" == "-n" ]]; then
+  NON_INTERACTIVE=true
+fi
 
 # network calc helper functions
 tonum() {
@@ -47,29 +87,74 @@ cidr2network() {
   fi
 }
 
-CCU_NETWORK_INTERFACE=$(ip -o -f inet route |grep -e "^default" | awk '{print $5}')
-read -r -e -p "HomeAssistant Main Ethernet Interface (e.g. eth0): " -i "${CCU_NETWORK_INTERFACE}" CCU_NETWORK_INTERFACE </dev/tty
+echo -n "HomeAssistant Main Ethernet Interface (e.g. eth0): "
+if [[ -z "${CCU_NETWORK_INTERFACE}" ]]; then
+  CCU_NETWORK_INTERFACE=$(ip -o -f inet route | grep -e "^default" | awk '{print $5}')
+  if [[ -z "${NON_INTERACTIVE}" ]]; then
+    read -r -e -i "${CCU_NETWORK_INTERFACE}" CCU_NETWORK_INTERFACE </dev/tty
+  else
+    echo "${CCU_NETWORK_INTERFACE}"
+  fi
+else
+  echo "${CCU_NETWORK_INTERFACE}"
+fi
 
-CCU_NETWORK_CIDR=$(ip -o -f inet addr show dev "${CCU_NETWORK_INTERFACE}" | awk '/scope global/ {print $4}')
-CCU_NETWORK_SUBNET=$(cidr2network "${CCU_NETWORK_CIDR}")
-read -r -e -p "HomeAssistant Main Subnet (e.g. 192.168.178.0/24): " -i "${CCU_NETWORK_SUBNET}" CCU_NETWORK_SUBNET </dev/tty
+echo -n "HomeAssistant Main Subnet (e.g. 192.168.178.0/24): "
+if [[ -z "${CCU_NETWORK_SUBNET}" ]]; then
+  CCU_NETWORK_CIDR=$(ip -o -f inet addr show dev "${CCU_NETWORK_INTERFACE}" | awk '/scope global/ {print $4}')
+  CCU_NETWORK_SUBNET=$(cidr2network "${CCU_NETWORK_CIDR}")
+  if [[ -z "${NON_INTERACTIVE}" ]]; then
+    read -r -e -i "${CCU_NETWORK_SUBNET}" CCU_NETWORK_SUBNET </dev/tty
+  else
+    echo "${CCU_NETWORK_SUBNET}"
+  fi
+else
+  echo "${CCU_NETWORK_SUBNET}"
+fi
 
-CCU_NETWORK_GATEWAY=$(ip route list dev "${CCU_NETWORK_INTERFACE}" | awk ' /^default/ {print $3}')
-read -r -e -p 'HomeAssistant Main Gateway (e.g. 192.168.178.1): ' -i "${CCU_NETWORK_GATEWAY}"  CCU_NETWORK_GATEWAY </dev/tty
+echo -n "HomeAssistant Main Gateway (e.g. 192.168.178.1): "
+if [[ -z "${CCU_NETWORK_GATEWAY}" ]]; then
+  CCU_NETWORK_GATEWAY=$(ip route list dev "${CCU_NETWORK_INTERFACE}" | awk ' /^default/ {print $3}')
+  if [[ -z "${NON_INTERACTIVE}" ]]; then
+    read -r -e -i "${CCU_NETWORK_GATEWAY}" CCU_NETWORK_GATEWAY </dev/tty
+  else
+    echo "${CCU_NETWORK_GATEWAY}"
+  fi
+else
+  echo "${CCU_NETWORK_GATEWAY}"
+fi
 
-CCU_CONTAINER_NAME=
-read -r -p 'RaspberryMatic Add-on Hostname (e.g. 5422eb72-raspberrymatic): ' CCU_CONTAINER_NAME </dev/tty
+echo -n "RaspberryMatic Add-on Hostname (e.g. 5422eb72-raspberrymatic): "
 if [[ -z "${CCU_CONTAINER_NAME}" ]]; then
-  echo "ERROR: Must specify the hostname of the running RaspberryMatic add-on"
-  exit 1
+  CCU_CONTAINER_NAME=$(docker ps --format '{{.Names}}' | grep "_raspberrymatic" | cut -c7-)
+  if [[ -z "${NON_INTERACTIVE}" ]]; then
+    read -r -e -i "${CCU_CONTAINER_NAME}" CCU_CONTAINER_NAME </dev/tty
+  else
+    echo "${CCU_CONTAINER_NAME}"
+  fi
+  if [[ -z "${CCU_CONTAINER_NAME}" ]]; then
+    echo "ERROR: Must specify the hostname of the running RaspberryMatic add-on"
+    exit 1
+  fi
+else
+  echo "${CCU_CONTAINER_NAME}"
 fi
 CCU_CONTAINER_NAME=$(echo "addon_${CCU_CONTAINER_NAME}" | sed 's/-rasp/_rasp/')
 
-CCU_CONTAINER_IP=$(echo "${CCU_NETWORK_GATEWAY}" | cut -d"." -f1-3)
-read -r -e -p 'RaspberryMatic Add-on IP (e.g. 192.168.178.4): ' -i "${CCU_CONTAINER_IP}." CCU_CONTAINER_IP </dev/tty
+echo -n "RaspberryMatic Add-on IP (e.g. 192.168.178.4): "
 if [[ -z "${CCU_CONTAINER_IP}" ]]; then
-  echo "ERROR: Must specify a free ip to assign to RaspberryMatic add-on"
-  exit 1
+  CCU_CONTAINER_IP=$(echo "${CCU_NETWORK_GATEWAY}" | cut -d"." -f1-3)
+  if [[ -z "${NON_INTERACTIVE}" ]]; then
+    read -r -e -i "${CCU_CONTAINER_IP}." CCU_CONTAINER_IP </dev/tty
+  else
+    echo "${CCU_CONTAINER_IP}"
+  fi
+  if [[ -z "${CCU_CONTAINER_IP}" ]] || [[ $(echo "${CCU_CONTAINER_IP}" | grep -o "\." | wc -l) -ne 3 ]]; then
+    echo "ERROR: Must specify a free and valid ip to assign to RaspberryMatic add-on"
+    exit 1
+  fi
+else
+  echo "${CCU_CONTAINER_IP}"
 fi
 
 # check if add-on is running

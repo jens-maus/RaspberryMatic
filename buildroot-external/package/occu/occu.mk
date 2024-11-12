b9e4718009b1a37a3c927ab4844a2b6740bc05f1
Jens Maus <mail@jens-maus.de>
2024-11-12 23:43:09 +0100

    update OCCU to latest 3.79.6-5 to fix issue with HmIP-MOD-RC8 (refs #2898)

--- buildroot-external/package/occu/occu.mk
+++ buildroot-external/package/occu/occu.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OCCU_VERSION = 3.79.6-4
+OCCU_VERSION = 3.79.6-5
 OCCU_SITE = $(call github,jens-maus,occu,$(OCCU_VERSION))
 OCCU_LICENSE = HMSL
 OCCU_LICENSE_FILES = LicenseDE.txt

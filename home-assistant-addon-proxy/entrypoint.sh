990c04392e7b55df89398c11354d489edb197386
Jens Maus <mail@jens-maus.de>
2024-11-15 17:09:45 +0100

    update proxy addon to use alpine 3.20 and use the /app workdir in general.

--- home-assistant-addon-proxy/entrypoint.sh
+++ home-assistant-addon-proxy/entrypoint.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-set -x
-tempio -conf /data/options.json -template /ha-proxy.js.gtpl -out /bin/ha-proxy.js
-chmod a+rx /bin/ha-proxy.js
-ls -la /usr/bin/node
-ls -la /bin/ha-proxy.js
-ls -la /
-ls -la /app/
-exec /bin/ha-proxy.js
+tempio -conf /data/options.json -template /app/ha-proxy.js.gtpl -out /app/ha-proxy.js
+chmod a+rx /app/ha-proxy.js
+exec /app/ha-proxy.js

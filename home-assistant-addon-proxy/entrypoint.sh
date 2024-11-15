f31e4bb65e7114c32d086a7943a8339018be2a6f
Jens Maus <mail@jens-maus.de>
2024-11-15 15:54:02 +0100

    more debug

--- home-assistant-addon-proxy/entrypoint.sh
+++ home-assistant-addon-proxy/entrypoint.sh
@@ -4,4 +4,6 @@ tempio -conf /data/options.json -template /ha-proxy.js.gtpl -out /bin/ha-proxy.j
 chmod a+rx /bin/ha-proxy.js
 ls -la /usr/bin/node
 ls -la /bin/ha-proxy.js
+ls -la /
+ls -la /app/
 exec /bin/ha-proxy.js

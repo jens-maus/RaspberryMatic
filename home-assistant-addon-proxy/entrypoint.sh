bb6688bd82fbccebcbb44f341dc0f0bd2dfee497
Jens Maus <mail@jens-maus.de>
2024-11-15 15:01:32 +0100

    add debug info and start ha-proxy.js via node binary

--- home-assistant-addon-proxy/entrypoint.sh
+++ home-assistant-addon-proxy/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 tempio -conf /data/options.json -template /ha-proxy.js.gtpl -out /bin/ha-proxy.js
 chmod a+rx /bin/ha-proxy.js
-exec /bin/ha-proxy.js
+ls -la /usr/bin/node
+ls -la /bin/ha-proxy.js
+exec /usr/bin/node /bin/ha-proxy.js

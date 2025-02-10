8d9b759376224603b120a3ab1f09a68313ec17e8
Jens Maus <mail@jens-maus.de>
2025-02-10 20:50:43 +0100

    update wiringpi-odroid to 3.17.0

--- buildroot-external/package/wiringpi-odroid/wiringpi-odroid.mk
+++ buildroot-external/package/wiringpi-odroid/wiringpi-odroid.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WIRINGPI_ODROID_VERSION = 58cea616e0a450fbcb8c148824f67609cce37b3d
+WIRINGPI_ODROID_VERSION = d29db23b9280e76a49138424377389bc5a265d40
 WIRINGPI_ODROID_SITE = $(call github,hardkernel,wiringPi,$(WIRINGPI_ODROID_VERSION))
 
 WIRINGPI_ODROID_LICENSE = LGPL-3.0+

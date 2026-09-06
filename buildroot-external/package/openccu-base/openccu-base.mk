399315941eecc9b7698d6334c6dd5a0bf510205f
Jens Maus <mail@jens-maus.de>
2026-09-06 21:53:01 +0200

    bump openccu-base to 4e43955b2aefd25158166a09915680be733af91d

--- buildroot-external/package/openccu-base/openccu-base.mk
+++ buildroot-external/package/openccu-base/openccu-base.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPENCCU_BASE_VERSION = 8772ccb3e7c4dd62371d1b85536ffc9da9fb8414
+OPENCCU_BASE_VERSION = 4e43955b2aefd25158166a09915680be733af91d
 OPENCCU_BASE_COMPAT_VERSION = 3.89.8
 OPENCCU_BASE_SITE = https://github.com/OpenCCU/OpenCCU-Base
 OPENCCU_BASE_SITE_METHOD = git
